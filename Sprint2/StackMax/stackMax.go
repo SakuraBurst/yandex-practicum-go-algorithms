19594324d7633f980b5389f96d62c145d280369b
SakuraBurst <colapes2@gmail.com>
2021-10-19 01:55:47 +0300

    stackmax

--- Sprint2/StackMax/stackMax.go
+++ Sprint2/StackMax/stackMax.go
@@ -1,1 +1,84 @@
 package stackmax
+
+import (
+	"bufio"
+	"errors"
+	"fmt"
+	"os"
+	"regexp"
+	"strconv"
+)
+
+type Stack []int
+
+func (s *Stack) pop() error {
+	if s.isEmpty() {
+		return errors.New("error")
+	}
+	*s = (*s)[:len(*s)-1]
+	return nil
+}
+func (s *Stack) push(i int) {
+	*s = append(*s, i)
+
+}
+func (s Stack) getMax() (int, error) {
+	if s.isEmpty() {
+		return 0, errors.New("None")
+	}
+	max := s[0]
+	if len(s) == 1 {
+		return max, nil
+	} else {
+		for i := 1; i < len(s); i++ {
+			if max < s[i] {
+				max = s[i]
+			}
+		}
+	}
+	return max, nil
+}
+
+func (s Stack) isEmpty() bool {
+	return len(s) == 0
+}
+
+var GET_MAX = regexp.MustCompile(`(?i)get_max`)
+var POP = regexp.MustCompile(`(?i)pop`)
+var PUSH = regexp.MustCompile(`(?i)^push`)
+var INT = regexp.MustCompile(`-?\d+`)
+
+func StackMax() {
+	var n int
+	fmt.Scan(&n)
+	stdReader := bufio.NewReader(os.Stdin)
+	stdReader.ReadByte()
+	stdScanner := bufio.NewScanner(stdReader)
+	stack := make(Stack, 0)
+	stdWriter := bufio.NewWriter(os.Stdout)
+	for i := 0; i < n; i++ {
+		stdScanner.Scan()
+		switch {
+		case GET_MAX.MatchString(stdScanner.Text()):
+			i, err := stack.getMax()
+			if err != nil {
+				stdWriter.WriteString(err.Error())
+				stdWriter.WriteByte('\n')
+			} else {
+				stdWriter.WriteString(strconv.Itoa(i))
+				stdWriter.WriteByte('\n')
+			}
+		case POP.MatchString(stdScanner.Text()):
+			err := stack.pop()
+			if err != nil {
+				stdWriter.WriteString(err.Error())
+				stdWriter.WriteByte('\n')
+			}
+		case PUSH.MatchString(stdScanner.Text()):
+			mm := INT.FindString(stdScanner.Text())
+			i, _ := strconv.Atoi(mm)
+			stack.push(i)
+		}
+	}
+	stdWriter.Flush()
+}
--- main.go
+++ main.go
@@ -1,38 +1,63 @@
 package main
 
-type ListNode struct {
-	data string
-	next *ListNode
-	prev *ListNode
+import (
+	"bytes"
+	"fmt"
+	"log"
+	"os"
+
+	stackmax "github.com/SakuraBurst/yandex-practicum-go-algorithms/Sprint2/StackMax"
+	"golang.org/x/net/html"
+)
+
+func main() {
+	stackmax.StackMax()
+	// buf := bytes.NewBuffer([]byte{})
+	// buf.WriteString(`<ul class="tabs-menu tabs-menu_theme_normal tabs-menu_layout_vert tabs-menu_size_m tabs-menu_role_problems inline-block i-bem tabs-menu_js_inited" data-bem="{&quot;tabs-menu&quot;:{}}" role="menu"><li class="tabs-menu__tab tabs-menu__tab_first_yes"><a class="link" href="/contest/22779/problems/A/"><span class="tabs-menu__tab-content-text">A. Мониторинг</span></a></li><li class="tabs-menu__tab tabs-menu__tab_active_yes"><a class="link" href="/contest/22779/problems/B/"><span class="tabs-menu__tab-content-text">B. Список дел</span></a></li><li class="tabs-menu__tab"><a class="link" href="/contest/22779/problems/C/"><span class="tabs-menu__tab-content-text">C. Нелюбимое дело</span></a></li><li class="tabs-menu__tab"><a class="link" href="/contest/22779/problems/D/"><span class="tabs-menu__tab-content-text">D. Заботливая мама</span></a></li><li class="tabs-menu__tab"><a class="link" href="/contest/22779/problems/E/"><span class="tabs-menu__tab-content-text">E. Всё наоборот</span></a></li><li class="tabs-menu__tab"><a class="link" href="/contest/22779/problems/F/"><span class="tabs-menu__tab-content-text">F. Стек - Max</span></a></li><li class="tabs-menu__tab"><a class="link" href="/contest/22779/problems/G/"><span class="tabs-menu__tab-content-text">G. Стек - MaxEffective</span></a></li><li class="tabs-menu__tab"><a class="link" href="/contest/22779/problems/H/"><span class="tabs-menu__tab-content-text">H. Скобочная последовательность</span></a></li><li class="tabs-menu__tab"><a class="link" href="/contest/22779/problems/I/"><span class="tabs-menu__tab-content-text">I. Ограниченная очередь</span></a></li><li class="tabs-menu__tab"><a class="link" href="/contest/22779/problems/J/"><span class="tabs-menu__tab-content-text">J. Списочная очередь</span></a></li><li class="tabs-menu__tab"><a class="link" href="/contest/22779/problems/K/"><span class="tabs-menu__tab-content-text">K. Рекурсивные числа Фибоначчи</span></a></li><li class="tabs-menu__tab"><a class="link" href="/contest/22779/problems/L/"><span class="tabs-menu__tab-content-text">L. Фибоначчи по модулю</span></a></li></ul>`)
+	// mainNode, err := html.Parse(buf)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// CreateAllDirectoriesAndFilesFromHtmlList(mainNode, "Sprint2")
+	// createFilesForNextSprint("Sprint2")
 }
 
-func Solution(head *ListNode) *ListNode {
-	if head.next == nil {
-		swap(head)
-		return head
+func createFilesForNextSprint(path string) {
+	dirs, err := os.ReadDir(path)
+	if err != nil {
+		log.Fatal(err)
 	}
-	result := Solution(head.next)
-	swap(head)
-	return result
-}
-func swap(node *ListNode) {
-	node.next, node.prev = node.prev, node.next
+	for _, v := range dirs {
+		name := v.Name()
+		f, err := os.Create(path + "/" + name + "/README.md")
+		if err != nil {
+			log.Fatal(err)
+		}
+		g, err := os.Create(path + "/" + name + "/" + string(bytes.ToLower([]byte(name[:1]))) + string([]byte(name[1:])) + ".go")
+
+		if err != nil {
+			log.Fatal(err)
+		}
+		err = f.Close()
+		if err != nil {
+			log.Fatal(err)
+		}
+		err = g.Close()
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
 }
 
-func test() {
-	node3 := ListNode{"node3", nil, nil}
-	node2 := ListNode{"node2", &node3, nil}
-	node1 := ListNode{"node1", &node2, nil}
-	node0 := ListNode{"node0", &node1, nil}
-	node3.prev = &node2
-	node2.prev = &node1
-	node1.prev = &node0
-	Solution(&node0)
-	// result is : newHead == node3
-	// node3.next == node2
-	// node2.next == node1
-	// node2.prev = node3
-	// node1.next == node0
-	// node1.prev == node2
-	// node0.prev == node1
+func CreateAllDirectoriesAndFilesFromHtmlList(node *html.Node, dir string) {
+	if node == nil {
+		return
+	}
+	if node.Type == html.TextNode {
+		os.Mkdir((dir + "/" + node.Data), os.ModePerm)
+		fmt.Println(node.Data)
+	}
+	CreateAllDirectoriesAndFilesFromHtmlList(node.FirstChild, dir)
+	CreateAllDirectoriesAndFilesFromHtmlList(node.NextSibling, dir)
+
 }
