9b06c9e1a26e4cd3cc3c51183f8532306a6d24b8
SakuraBurst <colapes2@gmail.com>
2021-10-21 00:01:56 +0300

    bracketSquense

--- Sprint2/BracketSequence/bracketSequence.go
+++ Sprint2/BracketSequence/bracketSequence.go
@@ -1,1 +1,90 @@
 package bracketsequence
+
+import (
+	"bufio"
+	"errors"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func BracketSequence() {
+	stdReader := bufio.NewReader(os.Stdin)
+	str, _ := stdReader.ReadString('\n')
+	str = strings.TrimSpace(str)
+	stack := make(Stack, 0, len(str))
+	for i := 0; i < len(str); i++ {
+		s := string(str[i])
+		if isOpenedBracket(s) {
+			stack.push(s)
+		} else {
+			if err := stack.popItem(toOpenedBracket(s)); err != nil {
+				fmt.Println("False")
+				return
+			}
+		}
+	}
+	if stack.isEmpty() {
+		fmt.Println("True")
+		return
+	} else {
+		fmt.Println("False")
+	}
+
+}
+
+func toOpenedBracket(b string) string {
+	switch b {
+	case "}":
+		return "{"
+	case ")":
+		return "("
+	case "]":
+		return "["
+	default:
+		return ""
+	}
+}
+
+func isOpenedBracket(b string) bool {
+	switch b {
+	case "{":
+		return true
+	case "(":
+		return true
+	case "[":
+		return true
+	default:
+		return false
+	}
+}
+
+type Stack []string
+
+// func (b *Stack) pop() error {
+// 	if b.isEmpty() {
+// 		return errors.New("stack is empty")
+// 	}
+// 	*b = (*b)[:len(*b)-1]
+// 	return nil
+// }
+
+func (b *Stack) popItem(i string) error {
+	if b.isEmpty() {
+		return errors.New("stack is empty")
+	}
+	deleteItem := (*b)[len(*b)-1]
+	if deleteItem != i {
+		return errors.New("items Is Not Equal " + deleteItem + " !== " + i)
+	}
+	*b = (*b)[:len(*b)-1]
+	return nil
+}
+
+func (b *Stack) push(s string) {
+	*b = append(*b, s)
+}
+
+func (b Stack) isEmpty() bool {
+	return len(b) == 0
+}
