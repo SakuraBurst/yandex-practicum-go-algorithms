ef1d1a6b8f25acc18888548aa9fb672bb69c39c4
SakuraBurst <colapes2@gmail.com>
2021-10-24 19:34:25 +0300

    fib

--- Sprint2/Fib/fib.go
+++ Sprint2/Fib/fib.go
@@ -1,1 +1,16 @@
 package fib
+
+import "fmt"
+
+func Fib() {
+	var n int
+	fmt.Scan(&n)
+	fmt.Println(nFib(n + 1))
+}
+
+func nFib(n int) int {
+	if n < 2 {
+		return n
+	}
+	return nFib(n-1) + nFib(n-2)
+}
