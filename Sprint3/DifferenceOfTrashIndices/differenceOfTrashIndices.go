450385ea79512935ed190550e4109f7e12ac15db
SakuraBurst <colapes2@gmail.com>
2021-11-15 11:49:47 +0300

    time limit difoftr

--- Sprint3/DifferenceOfTrashIndices/differenceOfTrashIndices.go
+++ Sprint3/DifferenceOfTrashIndices/differenceOfTrashIndices.go
@@ -1,13 +1,79 @@
 package differenceOfTrashIndices
 
 import (
-	"io"
 	"bufio"
-	"fmt"
+	"io"
+	"math"
+	"os"
+	"strconv"
+	"strings"
 )
 
-func DifferenceOfTrashIndices(r io.Reader, w io.Writer){
+func DifferenceOfTrashIndices(r io.Reader, w io.Writer) {
 	reader := bufio.NewReader(r)
+	nString, _ := reader.ReadString('\n')
+	n, _ := strconv.Atoi(strings.TrimSpace(nString))
+	stringData, _ := reader.ReadString('\n')
+	data := strings.Fields(stringData)
+	kString, _ := reader.ReadString('\n')
+	k, _ := strconv.Atoi(strings.TrimSpace(kString))
+	intData := make([]int, n)
+	for i, v := range data {
+		iV, _ := strconv.Atoi(v)
+		intData[i] = iV
+	}
+	minDiffs := make([]int, 0, k)
+	for i := 0; i < n; i++ {
+		for g := i + 1; g < n; g++ {
+			r := int(math.Abs(float64(intData[i] - intData[g])))
+			if len(minDiffs) < k {
+				minDiffs = append(minDiffs, r)
+				tupichSort(minDiffs)
+			} else {
+
+				if r < minDiffs[k-1] {
+					insertLow(minDiffs, r)
+
+				} else {
+					continue
+				}
+
+			}
+		}
+	}
 	writer := bufio.NewWriter(w)
-	fmt.Println(reader, writer)
+	writer.WriteString(strconv.Itoa(minDiffs[k-1]))
+	writer.Flush()
+}
+
+func insertLow(d []int, insert int) {
+	for i := len(d) - 1; i >= 0; i-- {
+		if insert < d[i] {
+			if i == len(d)-1 {
+				continue
+			} else {
+				d[i+1] = d[i]
+			}
+		} else {
+			d[i+1] = insert
+			return
+		}
+	}
+	if insert < d[0] {
+		d[0] = insert
+		return
+	}
+
+}
+
+func tupichSort(d []int) {
+	for i := 1; i < len(d); i++ {
+		for g := i; g > 0 && d[g] < d[g-1]; g-- {
+			d[g], d[g-1] = d[g-1], d[g]
+		}
+	}
+}
+
+func main() {
+	DifferenceOfTrashIndices(os.Stdin, os.Stdout)
 }
--- Sprint3/DifferenceOfTrashIndices/differenceOfTrashIndices_test.go
+++ Sprint3/DifferenceOfTrashIndices/differenceOfTrashIndices_test.go
@@ -14,8 +14,18 @@ type Test struct {
 	outputData string
 }
 
-func TestDifferenceOfTrashIndices(t *testing.T){
-	tests := []Test{{strings.NewReader(""), ""}}
+func TestDifferenceOfTrashIndices(t *testing.T) {
+	tests := []Test{{strings.NewReader(`3
+2 3 4
+2`), "1"}, {strings.NewReader(`3
+1 3 1
+1`), "0"}, {strings.NewReader(`3
+1 3 5
+3`), "4"}, {strings.NewReader(`4
+9 6 10 3
+3`), "3"}, {strings.NewReader(`6
+27 67 3 4 17 98
+7`), "31"}}
 	for _, v := range tests {
 		buf := bytes.NewBuffer([]byte{})
 		differenceOfTrashIndices.DifferenceOfTrashIndices(v.inputData, buf)
