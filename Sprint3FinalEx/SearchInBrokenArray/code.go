22b008dc665d0cfd7aedc210d2f09c35b47ad1e5
SakuraBurst <colapes2@gmail.com>
2021-11-18 19:39:28 +0300

    broken Search complete

--- Sprint3FinalEx/SearchInBrokenArray/code.go
+++ Sprint3FinalEx/SearchInBrokenArray/code.go
@@ -1,48 +1,174 @@
 package brokenarray
 
 import (
-	"fmt"
 	"math"
 )
 
 func BrokenSearch(arr []int, k int) int {
+	// главная проверка. Если искомое число больше, чем последний элемент массива, то, условно, мы будем искать в правой части, если нет - в левой
+	isKHigerThenLastElement := k > arr[len(arr)-1]
+	result := -1
+	start := 0
+	end := len(arr)
+	// если результат не найден за логарифмическое вермя, значит его нет и result остается -1
+	// почему-то ингода он не успевает найти, поэтому я добавли + 1 шаг, с чем связано без понятия
+	logArrLenght := int(math.Log2(float64(len(arr))))
+	for i := 0; i <= logArrLenght; i++ {
+		// часто замечал в исходниках го, что там деление на 2 производится с помощью шифта вправо
+		// может это бысрее чем обычное деление на 2? Я не знаю :D Просто копирую)
+		middle := (start + end) >> 1
+		if arr[middle] == k {
+			result = middle
+			break
+		}
+		if arr[middle] > k {
+			if isKHigerThenLastElement {
+				end = middle
+			} else {
+				if arr[middle] > arr[len(arr)-1] {
+					start = middle
+				} else {
+					end = middle
+				}
+			}
+		} else {
+			if isKHigerThenLastElement {
+				if arr[middle] < arr[len(arr)-1] {
+					end = middle
+				} else {
+					start = middle
+				}
+			} else {
+				start = middle
+			}
+		}
+	}
+
+	return result
+}
+
+/*
+для примера возьмем массив
+[7,8,9,10,1,2,3,4,5,6]
+
+Если выбранное число > k && k > последний элемент массива {
+	искомое число не может быть правее, потому что самое большое число справа меньше чем k
+	то тогда идем левее
+	пример:
+	k = 9
+	guess = 10
+	мы теперь ищем в [7,8,9]
+}
+
+Если выбранное число > k && k < последний элемент массива {
+	плохой исход, так как если k меньше поледнего элемента, то числа больше могут быть как слева, так и справа
+	придется делать проверку
+	Если выбранное число > последний элемент массива {
+		ищем в правой части
+		пример:
+		k = 5
+		guess = 10
+		мы теперь ищем в [1,2,3,4,5,6]
+	} В ином случае {
+		ищем в левой части
+		пример:
+		k = 5
+		guess 6
+		мы теперь ищем в [7,8,9,10,1,2,3,4,5]
+	}
+}
+
+Если выбранное число < k && k > последний элемент массива {
+	плохой исход, так как если k больше поледнего элемента, то числа меньше могут быть как слева, так и справа
+	плохой исход, придется делать две проверки
+	Если выбранное число < последний элемент массива {
+		ищем в левой части
+		пример:
+		k = 10
+		guess = 2
+		мы теперь ищем в [7,8,9,10,1]
+	} В ином случае {
+		ищем в правой части
+		пример:
+		k = 10
+		guess 7
+		мы теперь ищем в [8,9,10,1,2,3,4,5,6]
+	}
+
+	то тогда мы ищем правее
+	пример:
+	k = 9
+	guess = 8
+	мы теперь ищем в [9,10,1,2,3,4,5,6]
+}
+
+
+
+Если выбранное число < k && k < последний элемент массива {
+	если k < последний элемент массива, то числа меньше k могут быть только справа
+	то тогда ищем слева
+	пример k = 4
+	guess = 3
+	мы теперь ищем в [,4,5,6]
+}
+
+
+
+*/
+
+func BrokenSearch2(arr []int, k int) int {
 	// Your code
 	// “ヽ(´▽｀)ノ”
 	arrStart := 0
-	guess := len(arr) >> 1
-	// проверяем, что массив сбился, если не сиблся то arrStart остается 0
-	if arr[len(arr)-1] < arr[0] {
-		counter := 0
-		for guess == 0 {
-			counter++
+	if len(arr) == 1 {
+		if arr[0] == k {
+			return 0
+		} else {
+			return -1
+		}
+	}
+	// проверяем на то, что начало массива в самом конце потому что это O(1) и мы можем сэкономить время
+	if arr[len(arr)-1] < arr[len(arr)-2] {
+		arrStart = len(arr) - 1
+		// потом проверяем, что массив сбился, если не сиблся то arrStart остается 0
+	} else if arr[len(arr)-1] < arr[0] {
+		start := 0
+		end := len(arr)
+		for arrStart == 0 {
+			guess := (start + end) >> 1
 			if arr[guess] < arr[len(arr)-1] {
 				if arr[guess] < arr[guess-1] {
 					arrStart = guess
 				} else {
-					guess >>= 1
+					end = guess
 				}
 			} else {
-				guess += guess >> 1
+				start = guess
 			}
 		}
-		fmt.Println(counter)
 	}
 
-	guess = len(arr) >> 1
+	// fmt.Println("nachalo massiva nahoditsya po indexsu = ", arrStart, "i ravno = ", arr[arrStart])
 	result := -1
-	for i := 0; i < int(math.Log2(float64(len(arr)))); i++ {
-		fixedGuess := (guess + arrStart) % len(arr)
+	start := 0
+	end := len(arr)
+	// если результат не найден за логарифмическое вермя, значит его нет
+	// почему-то ингода он не успевает найти, поэтому я добавли + 1 шаг, с чем связано без понятия
+	logArrLenght := int(math.Log2(float64(len(arr))))
+	for i := 0; i <= logArrLenght; i++ {
+		guess := (start + end) >> 1
+		fixedGuess := ((guess + arrStart) % len(arr))
 		if arr[fixedGuess] == k {
 			result = fixedGuess
 			break
 		}
 		if arr[fixedGuess] < k {
-			guess += guess >> 1
+			start = guess
 		} else {
-			guess >>= 1
+			end = guess
 		}
 
 	}
 
-	return result + 1
+	return result
 }
--- Sprint3FinalEx/SearchInBrokenArray/code_test.go
+++ Sprint3FinalEx/SearchInBrokenArray/code_test.go
@@ -8,8 +8,10 @@ import (
 )
 
 func Test(t *testing.T) {
+	//            0   1    2    3    4    5    6    7    8    9   10  11 12 13 14 15
 	arr := []int{19, 21, 100, 101, 109, 112, 113, 114, 115, 116, 153, 1, 4, 5, 7, 12}
-	if brokenarray.BrokenSearch(arr, 5) != 6 {
+	// arr = []int{10, 1, 2, 3, 4, 5, 6, 7, 8, 9}
+	if brokenarray.BrokenSearch(arr, 5) != 13 {
 		fmt.Println(brokenarray.BrokenSearch(arr, 5), "kachalka")
 		panic("WA")
 	}
