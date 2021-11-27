ee0fc3df9597eb3fd1b832b6a80eea16fde1e497
SakuraBurst <colapes2@gmail.com>
2021-11-27 03:16:23 +0300

    refactor

--- Sprint4/PrefixHash/prefixHash.go
+++ Sprint4/PrefixHash/prefixHash.go
@@ -2,7 +2,6 @@ package prefixHash
 
 import (
 	"bufio"
-	"fmt"
 	"io"
 	"os"
 	"strconv"
@@ -18,29 +17,13 @@ func PrefixHash(r io.Reader, w io.Writer) {
 	m, _ := strconv.Atoi(strings.TrimSpace(mString))
 	s, _ := reader.ReadString('\n')
 	s = strings.TrimSpace(s)
+
+	degreeOfA := nDegreesByModule(a, m, len(s))
+	pref := prefixHashesOfString(s, a, m)
 	scaner := bufio.NewScanner(reader)
+
 	scaner.Scan()
 	t, _ := strconv.Atoi(scaner.Text())
-	// hashOfIndexRanges := map[[2]int]int{}
-	// hashOfIndexRanges[[2]int{0, len(s) - 1}] = hash([]byte(s), a, m)
-	degreeOfA := make([]int, 0, t)
-	degreeOfA = append(degreeOfA, 1)
-	lenS := len(s)
-	for i := 1; i <= lenS; i++ {
-		degree := degreeOfA[i-1] * a
-		if degree < 0 {
-			degreeOfA = append(degreeOfA, (degree%m+m)%m)
-		} else {
-			degreeOfA = append(degreeOfA, degree%m)
-		}
-
-	}
-	pref := make([]int, lenS+1)
-	pref[0] = 0
-	for i := 1; i < lenS+1; i++ {
-		pref[i] = (int(s[i-1]) + pref[i-1]*a) % m
-	}
-	fmt.Println(a, m, s, degreeOfA, pref)
 	writer := bufio.NewWriter(w)
 	for i := 0; i < t; i++ {
 		scaner.Scan()
@@ -52,7 +35,6 @@ func PrefixHash(r io.Reader, w io.Writer) {
 			writer.WriteByte('\n')
 		} else {
 			v := (pref[bI] - pref[aI-1]*degreeOfA[bI-aI+1])
-			fmt.Println(aI, bI, degreeOfA[bI-aI])
 			if v < 0 {
 				v %= m
 				v += m
@@ -67,17 +49,30 @@ func PrefixHash(r io.Reader, w io.Writer) {
 
 }
 
-// func hash(s string, a, m int) int {
-// 	// fmt.Println(s)
-// 	if len(s) == 1 {
-// 		return int(s[0]) % m
-// 	}
-// 	result := ((int(s[0]) * a) + int(s[1])) % m
-// 	for i := 2; i < len(s); i++ {
-// 		result = ((result * a) + int(s[i])) % m
-// 	}
-// 	return result
-// }
+func nDegreesByModule(val, mod, n int) []int {
+	degrees := make([]int, n+1)
+	degrees[0] = 1
+	for i := 1; i <= n; i++ {
+		res := degrees[i-1] * val
+		degrees[i] = res % mod
+		// подумоть
+		// if val > 0 {
+
+		// } else {
+		// 	degrees[i] = ((res % mod) + mod) % mod
+		// }
+	}
+	return degrees
+}
+
+func prefixHashesOfString(s string, a, m int) []int {
+	pref := make([]int, len(s)+1)
+	pref[0] = 0
+	for i := 1; i <= len(s); i++ {
+		pref[i] = (pref[i-1]*a + int(s[i-1])) % m
+	}
+	return pref
+}
 
 func main() {
 	PrefixHash(os.Stdin, os.Stdout)
