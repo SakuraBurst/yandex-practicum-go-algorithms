bc8a321928b250352ec9fe3be4181d93f1c26d1e
SakuraBurst <colapes2@gmail.com>
2021-12-01 01:30:17 +0300

    anagram grouping

--- Sprint4/AnagramGrouping/anagramGrouping.go
+++ Sprint4/AnagramGrouping/anagramGrouping.go
@@ -1,13 +1,83 @@
 package anagramGrouping
 
 import (
-	"io"
 	"bufio"
-	"fmt"
+	"io"
+	"os"
+	"strconv"
+	"strings"
 )
 
-func AnagramGrouping(r io.Reader, w io.Writer){
+func AnagramGrouping(r io.Reader, w io.Writer) {
 	reader := bufio.NewReader(r)
-	//writer := bufio.NewWriter(w)
-	fmt.Println(reader)
+	reader.ReadString('\n')
+	s, _ := reader.ReadString('\n')
+	sData := strings.Fields(s)
+	hashMap := map[int][]int{}
+	for i, v := range sData {
+		hashMap[hash(v)] = append(hashMap[hash(v)], i)
+	}
+	resultSlice := make([][]int, 0, len(hashMap))
+	for _, v := range hashMap {
+		resultSlice = append(resultSlice, v)
+	}
+	resultSlice = simpleMerge(resultSlice)
+	writer := bufio.NewWriter(w)
+	for _, rv := range resultSlice {
+		for i, v := range rv {
+			writer.WriteString(strconv.Itoa(v))
+			if len(rv)-1 > i {
+				writer.WriteString(" ")
+			}
+		}
+		writer.WriteByte('\n')
+
+	}
+	writer.Flush()
+}
+
+func simpleMerge(d [][]int) [][]int {
+	if len(d) == 1 {
+		return d
+	}
+
+	mid := len(d) >> 1
+
+	a := simpleMerge(d[:mid])
+	b := simpleMerge(d[mid:])
+	aIndex := 0
+	bIndex := 0
+	result := make([][]int, 0, len(a)+len(b))
+	for aIndex < len(a) || bIndex < len(b) {
+		if aIndex == len(a) {
+			result = append(result, b[bIndex])
+			bIndex++
+			continue
+		}
+		if bIndex == len(b) {
+			result = append(result, a[aIndex])
+			aIndex++
+			continue
+		}
+		if a[aIndex][0] <= b[bIndex][0] {
+			result = append(result, a[aIndex])
+			aIndex++
+		} else {
+			result = append(result, b[bIndex])
+			bIndex++
+		}
+	}
+	return result
+}
+
+func hash(s string) int {
+	hash := 1
+	for i := 0; i < len(s); i++ {
+		hash = hash * int(s[i]) % 1000007
+	}
+	return hash
+}
+
+func main() {
+	AnagramGrouping(os.Stdin, os.Stdout)
 }
--- Sprint4/AnagramGrouping/anagramGrouping_test.go
+++ Sprint4/AnagramGrouping/anagramGrouping_test.go
@@ -14,8 +14,94 @@ type Test struct {
 	outputData string
 }
 
-func TestAnagramGrouping(t *testing.T){
-	tests := []Test{{strings.NewReader(""), ""}}
+func TestAnagramGrouping(t *testing.T) {
+	tests := []Test{{strings.NewReader(`6
+tan eat tea ate nat bat`), `0 4
+1 2 3
+5
+`}, {strings.NewReader(`81
+rjvew ivbmo kwfem zeboo pcdkw tfsbz yuwtm wmxjw nrqus nnqpp gcwpw ornbs pthyj yjolz rvnjg ybqwr hwpes mgzas juliq dyhhz njxgb bwzoh tosuk wobdr gnalw cuorl vukpl xkumi mkzph kkgqj uspch jluhu iljup fjuvk krwsd whpdw eixvf wjxmp jbldf fcxpk htcae vomzc fosvw hoplx fbqzl sgrnk bvywp crlmx twzih wlett bbvvc vbaej ljckp ohidr jzxng emspk dsjpq irvzq ysvlv wcity djyqw ehtqq kpvyp khucp bkulg wzbna fqmri nsaqs tohsd qrdxv bwlty bvpyr cxtds cqhbe nyzak mkebf jadyu sbrpk zykgr eqjns gfhob`), `0
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+19
+20
+21
+22
+23
+24
+25
+26
+27
+28
+29
+30
+31
+32
+33
+34
+35
+36
+37
+38
+39
+40
+41
+42
+43
+44
+45
+46
+47
+48
+49
+50
+51
+52
+53
+54
+55
+56
+57
+58
+59
+60
+61
+62
+63
+64
+65
+66
+67
+68
+69
+70
+71
+72
+73
+74
+75
+76
+77
+78
+79
+80
+`}}
 	for _, v := range tests {
 		buf := bytes.NewBuffer([]byte{})
 		anagramGrouping.AnagramGrouping(v.inputData, buf)
