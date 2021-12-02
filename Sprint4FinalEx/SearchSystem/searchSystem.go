0f4db03db0479ff48c7b4aff30c29ae07122b7a1
SakuraBurst <colapes2@gmail.com>
2021-12-03 02:25:54 +0300

    search system

--- Sprint4FinalEx/SearchSystem/searchSystem.go
+++ Sprint4FinalEx/SearchSystem/searchSystem.go
@@ -8,9 +8,9 @@ import (
 	"strings"
 )
 
-type WordLocation struct {
-	docIndex  int
-	wordIndex int
+type SearchResult struct {
+	index      int
+	wordsFound int
 }
 
 func SearchSystem(r io.Reader, w io.Writer) {
@@ -18,37 +18,42 @@ func SearchSystem(r io.Reader, w io.Writer) {
 	scaner := bufio.NewScanner(reader)
 	scaner.Scan()
 	nDocks, _ := strconv.Atoi(scaner.Text())
-	searchIndexesMap := map[string][]WordLocation{}
+	searchIndexesMap := map[string]map[int]int{}
 	for d := 0; d < nDocks; d++ {
 		scaner.Scan()
-		for w, v := range strings.Fields(scaner.Text()) {
-			searchIndexesMap[v] = append(searchIndexesMap[v], WordLocation{docIndex: d, wordIndex: w})
+		for _, v := range strings.Fields(scaner.Text()) {
+			if searchIndexesMap[v] == nil {
+				searchIndexesMap[v] = map[int]int{}
+			}
+			searchIndexesMap[v][d]++
 		}
 	}
 	scaner.Scan()
 	kSearchStrings, _ := strconv.Atoi(scaner.Text())
 	writer := bufio.NewWriter(w)
-	// mapOfResults = map[string]map[int]int{}
 	for s := 0; s < kSearchStrings; s++ {
 		scaner.Scan()
 		result := make(map[int]int, len(scaner.Text()))
 		alreadyCounted := make(map[string]bool)
 		for _, w := range strings.Fields(scaner.Text()) {
 			if !alreadyCounted[w] {
-				for _, wl := range searchIndexesMap[w] {
-					result[wl.docIndex]++
+				for k, wl := range searchIndexesMap[w] {
+					result[k] += wl
 				}
 				alreadyCounted[w] = true
 			}
 
 		}
-		counter := 0
-		for k := range result {
-			if counter == 5 {
+		counterArr := make([]SearchResult, 0, len(result))
+		for k, v := range result {
+			counterArr = append(counterArr, SearchResult{index: k + 1, wordsFound: v})
+		}
+		counterArr = merge(counterArr)
+		for i, v := range counterArr {
+			if i == 5 {
 				break
 			}
-			counter++
-			writer.WriteString(strconv.Itoa(k + 1))
+			writer.WriteString(strconv.Itoa(v.index))
 			writer.WriteByte(' ')
 		}
 		writer.WriteByte('\n')
@@ -56,6 +61,48 @@ func SearchSystem(r io.Reader, w io.Writer) {
 	}
 	writer.Flush()
 	//
+}
+
+func merge(d []SearchResult) []SearchResult {
+	if len(d) <= 1 {
+		return d
+	}
+	mid := len(d) >> 1
+	firstHalf := merge(d[:mid])
+	secondHalf := merge(d[mid:])
+	firstHalfIndex := 0
+	secondHalfIndex := 0
+	result := make([]SearchResult, 0, len(firstHalf)+len(secondHalf))
+	for firstHalfIndex < len(firstHalf) || secondHalfIndex < len(secondHalf) {
+		// если закончилась первая плоловина
+		if firstHalfIndex == len(firstHalf) {
+			result = append(result, secondHalf[secondHalfIndex])
+			secondHalfIndex++
+			continue
+		}
+		// если закончилась вторая половина
+		if secondHalfIndex == len(secondHalf) {
+			result = append(result, firstHalf[firstHalfIndex])
+			firstHalfIndex++
+			continue
+		}
+		// если обе половины что-то содержат
+		if compareResults(firstHalf[firstHalfIndex], secondHalf[secondHalfIndex]) {
+			result = append(result, firstHalf[firstHalfIndex])
+			firstHalfIndex++
+		} else {
+			result = append(result, secondHalf[secondHalfIndex])
+			secondHalfIndex++
+		}
+	}
+	return result
+}
+
+func compareResults(a, b SearchResult) bool {
+	if a.wordsFound == b.wordsFound {
+		return a.index < b.index
+	}
+	return a.wordsFound > b.wordsFound
 }
 
 func main() {
--- Sprint4FinalEx/SearchSystem/searchSystem_test.go
+++ Sprint4FinalEx/SearchSystem/searchSystem_test.go
@@ -23,9 +23,9 @@ free tea for everyone
 i like black coffee without milk
 everyone loves new year
 mary likes black coffee without milk
-`), `1 2
-3
-2 1
+`), `1 2 
+3 
+2 1 
 `}, {strings.NewReader(`6
 buy flat in moscow
 rent flat in moscow
@@ -35,7 +35,29 @@ clean flat in moscow on weekends
 renovate flat in moscow
 1
 flat in moscow for crazy weekends
-`), `4 5 1 2 3
+`), `4 5 1 2 3 
+`}, {strings.NewReader(`10
+ing ioizzlux knjklikxxq
+lntw qinx iltxhmrmfp
+kmsjgmfyu gic xivg pu
+nlbudxrvnv vrbs spdeuoqr
+eyew sosx iecoujab xtokvidtwf
+spdeuoqr qpcmtqzs ioizzlux
+jeqoseryf cvoafqw hnovlrgzp
+uanxvs gic ljymhdwgw
+spdeuoqr pu xivg mjotkrq
+rg uqblrnrgwr pu aeiv
+5
+uqblrnrgwr
+rg cvoafqw
+vrbs xivg pu
+kqweuyzgkt
+rg gic xivg
+`), `10 
+7 10 
+3 9 4 10 
+
+3 8 9 10 
 `}}
 	for _, v := range tests {
 		buf := bytes.NewBuffer([]byte{})
