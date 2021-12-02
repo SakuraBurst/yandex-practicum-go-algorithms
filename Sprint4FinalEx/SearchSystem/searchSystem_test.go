a0652e56f5a5c7e3af560866acef62d1a9cbfbcd
SakuraBurst <colapes2@gmail.com>
2021-12-02 19:41:55 +0300

    aszdfjkl;

--- Sprint4FinalEx/SearchSystem/searchSystem_test.go
+++ Sprint4FinalEx/SearchSystem/searchSystem_test.go
@@ -14,8 +14,29 @@ type Test struct {
 	outputData string
 }
 
-func TestSearchSystem(t *testing.T){
-	tests := []Test{{strings.NewReader(""), ""}}
+func TestSearchSystem(t *testing.T) {
+	tests := []Test{{strings.NewReader(`3
+i love coffee
+coffee with milk and sugar
+free tea for everyone
+3
+i like black coffee without milk
+everyone loves new year
+mary likes black coffee without milk
+`), `1 2
+3
+2 1
+`}, {strings.NewReader(`6
+buy flat in moscow
+rent flat in moscow
+sell flat in moscow
+want flat in moscow like crazy
+clean flat in moscow on weekends
+renovate flat in moscow
+1
+flat in moscow for crazy weekends
+`), `4 5 1 2 3
+`}}
 	for _, v := range tests {
 		buf := bytes.NewBuffer([]byte{})
 		searchSystem.SearchSystem(v.inputData, buf)
