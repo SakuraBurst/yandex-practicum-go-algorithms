245db33bf0fffb1d69e2bba8fec5fe89913c56cc
SakuraBurst <colapes2@gmail.com>
2021-12-03 16:16:55 +0300

    govno

--- Sprint4FinalEx/HashTable/hashTable.go
+++ Sprint4FinalEx/HashTable/hashTable.go
@@ -1,13 +1,66 @@
 package hashTable
 
 import (
-	"io"
 	"bufio"
 	"fmt"
+	"io"
+	"strconv"
+	"strings"
 )
 
-func HashTable(r io.Reader, w io.Writer){
+type KeyValue struct {
+	key   int
+	value int
+}
+
+type HashMap []KeyValue
+
+func (hm HashMap) hash(key int) int {
+	return 1
+}
+
+func HashTable(r io.Reader, w io.Writer) {
 	reader := bufio.NewReader(r)
+	scaner := bufio.NewScanner(reader)
+	scaner.Scan()
+	nCommands, _ := strconv.Atoi(scaner.Text())
+	primeForHash := getPrimeForN(nCommands)
+
+	fmt.Println(primeForHash)
+	for i := 0; i < nCommands; i++ {
+		scaner.Scan()
+		command := strings.Fields(scaner.Text())
+		switch {
+		case command[0] == "get":
+			getKey, _ := strconv.Atoi(command[1])
+			fmt.Println("get ", getKey)
+		case command[0] == "put":
+			putKey, _ := strconv.Atoi(command[1])
+			putValue, _ := strconv.Atoi(command[2])
+			fmt.Println("put ", putKey, putValue)
+		case command[0] == "delete":
+			deleteKey, _ := strconv.Atoi(command[1])
+			fmt.Println("delete ", deleteKey)
+		}
+	}
 	//writer := bufio.NewWriter(w)
 	fmt.Println(reader)
+}
+
+func getPrimeForN(n int) int {
+	switch {
+	case n < 10:
+		return 13
+	case n < 100:
+		return 107
+	case n < 1000:
+		return 1009
+	case n < 10000:
+		return 10007
+	case n < 100000:
+		return 100003
+	case n < 1000000:
+		return 1000003
+	}
+	return 3
 }
--- Sprint4FinalEx/HashTable/hashTable_test.go
+++ Sprint4FinalEx/HashTable/hashTable_test.go
@@ -14,8 +14,40 @@ type Test struct {
 	outputData string
 }
 
-func TestHashTable(t *testing.T){
-	tests := []Test{{strings.NewReader(""), ""}}
+func TestHashTable(t *testing.T) {
+	tests := []Test{{strings.NewReader(`214
+get 1
+put 1 10
+put 2 4
+get 1
+get 2
+delete 2
+get 2
+put 1 5
+get 1
+delete 2
+`), `None
+10
+4
+4
+None
+5
+None
+`}, {strings.NewReader(`8
+get 9
+delete 9
+put 9 1
+get 9
+put 9 2
+get 9
+put 9 3
+get 9
+`), `None
+None
+1
+2
+3
+`}}
 	for _, v := range tests {
 		buf := bytes.NewBuffer([]byte{})
 		hashTable.HashTable(v.inputData, buf)
