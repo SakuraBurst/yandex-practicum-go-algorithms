81e12841a07ddff1f1ff83ff8d9a44772c85af22
SakuraBurst <colapes2@gmail.com>
2021-12-06 21:55:54 +0300

    search tree

--- Sprint5/SearchTree/searchTree.go
+++ Sprint5/SearchTree/searchTree.go
@@ -1,13 +1,44 @@
 package searchTree
 
-import (
-	"io"
-	"bufio"
-	"fmt"
-)
+import "errors"
 
-func SearchTree(r io.Reader, w io.Writer){
-	reader := bufio.NewReader(r)
-	//writer := bufio.NewWriter(w)
-	fmt.Println(reader)
+type Node struct {
+	Value int
+	Left  *Node
+	Right *Node
+}
+
+func Solution(root *Node) bool {
+	// Your code
+	// “ヽ(´▽｀)ノ”
+	_, err := isSearchTree(root)
+	if err != nil {
+		return false
+	}
+	return true
+}
+
+func isSearchTree(node *Node) ([]int, error) {
+	if node == nil {
+		return []int{}, nil
+	}
+	if node.Right == nil && node.Left == nil {
+		return []int{node.Value}, nil
+	}
+	leftValues, leftErr := isSearchTree(node.Left)
+	rightValues, rightErr := isSearchTree(node.Right)
+	if leftErr != nil || rightErr != nil {
+		return nil, errors.New("wrong tree")
+	}
+	for _, v := range leftValues {
+		if v >= node.Value {
+			return nil, errors.New("wrong tree")
+		}
+	}
+	for _, v := range rightValues {
+		if v <= node.Value {
+			return nil, errors.New("wrong tree")
+		}
+	}
+	return append(leftValues, rightValues...), nil
 }
--- Sprint5/SearchTree/searchTree_test.go
+++ Sprint5/SearchTree/searchTree_test.go
@@ -1,26 +1,31 @@
 package searchTree_test
 
 import (
-	"bytes"
-	"io"
-	"strings"
 	"testing"
 
 	searchTree "github.com/SakuraBurst/yandex-practicum-go-algorithms/Sprint5/SearchTree"
 )
 
 type Test struct {
-	inputData  io.Reader
-	outputData string
+	inputData  searchTree.Node
+	outputData bool
 }
 
-func TestSearchTree(t *testing.T){
-	tests := []Test{{strings.NewReader(""), ""}}
+func TestSearchTree(t *testing.T) {
+	node1 := searchTree.Node{1, nil, nil}
+	node2 := searchTree.Node{4, nil, nil}
+	node2Wrong := searchTree.Node{5, nil, nil}
+	node3 := searchTree.Node{3, &node1, &node2}
+	node3Wrong := searchTree.Node{3, &node1, &node2Wrong}
+	node69 := searchTree.Node{9, nil, nil}
+	node4 := searchTree.Node{8, nil, &node69}
+	trueNode := searchTree.Node{5, &node3, &node4}
+	falseNode := searchTree.Node{5, &node3Wrong, &node4}
+	tests := []Test{{trueNode, true}, {falseNode, false}}
 	for _, v := range tests {
-		buf := bytes.NewBuffer([]byte{})
-		searchTree.SearchTree(v.inputData, buf)
-		if buf.String() != v.outputData {
-			t.Errorf("\nexpected:\n%s\ngot:\n%s", v.outputData, buf.String())
+		res := searchTree.Solution(&v.inputData)
+		if v.outputData != res {
+			t.Errorf("\nexpected:\n%t\ngot:\n%t", v.outputData, res)
 		}
 	}
 }
