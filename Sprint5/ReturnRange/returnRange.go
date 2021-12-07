7b02b00bb86cc538e028087decda106db3286eb7
SakuraBurst <colapes2@gmail.com>
2021-12-08 01:47:00 +0300

    return range

--- Sprint5/ReturnRange/returnRange.go
+++ Sprint5/ReturnRange/returnRange.go
@@ -1,13 +1,37 @@
 package returnRange
 
-import (
-	"io"
-	"bufio"
-	"fmt"
-)
+import "fmt"
 
-func ReturnRange(r io.Reader, w io.Writer){
-	reader := bufio.NewReader(r)
-	//writer := bufio.NewWriter(w)
-	fmt.Println(reader)
+type Node struct {
+	Value int
+	Left  *Node
+	Right *Node
+}
+
+func PrintRange(root *Node, left int, right int) {
+	if root.Left == nil && root.Right == nil {
+		if root.Value >= left && root.Value <= right {
+			fmt.Print(root.Value)
+			fmt.Print(" ")
+		}
+		return
+	}
+	if root.Left != nil && root.Value >= left {
+		PrintRange(root.Left, left, right)
+		if root.Value <= right {
+			fmt.Print(root.Value)
+			fmt.Print(" ")
+		}
+
+	}
+	if root.Right != nil && root.Value <= right {
+		if root.Value >= left && root.Left == nil {
+			fmt.Print(root.Value)
+			fmt.Print(" ")
+		}
+		PrintRange(root.Right, left, right)
+
+	}
+	// Your code
+	// “ヽ(´▽｀)ノ”
 }
--- Sprint5/ReturnRange/returnRange_test.go
+++ Sprint5/ReturnRange/returnRange_test.go
@@ -1,26 +1,32 @@
 package returnRange_test
 
 import (
-	"bytes"
-	"io"
-	"strings"
+	"fmt"
 	"testing"
 
 	returnRange "github.com/SakuraBurst/yandex-practicum-go-algorithms/Sprint5/ReturnRange"
 )
 
-type Test struct {
-	inputData  io.Reader
-	outputData string
-}
+func TestReturnRange(t *testing.T) {
+	node11 := returnRange.Node{2, nil, nil}
+	node12 := returnRange.Node{1, nil, &node11}
+	node13 := returnRange.Node{8, nil, nil}
+	node14 := returnRange.Node{8, nil, &node13}
+	node15 := returnRange.Node{9, &node14, nil}
+	node16 := returnRange.Node{10, &node15, nil}
+	node17 := returnRange.Node{5, &node12, &node16}
 
-func TestReturnRange(t *testing.T){
-	tests := []Test{{strings.NewReader(""), ""}}
-	for _, v := range tests {
-		buf := bytes.NewBuffer([]byte{})
-		returnRange.ReturnRange(v.inputData, buf)
-		if buf.String() != v.outputData {
-			t.Errorf("\nexpected:\n%s\ngot:\n%s", v.outputData, buf.String())
-		}
-	}
+	node21 := returnRange.Node{15, nil, nil}
+	node23 := returnRange.Node{1, nil, nil}
+	node22 := returnRange.Node{3, &node23, &node11}
+	node24 := returnRange.Node{11, nil, &node21}
+	node25 := returnRange.Node{7, &node22, &node24}
+
+	returnRange.PrintRange(&node17, 2, 8)
+	fmt.Println()
+	returnRange.PrintRange(&node25, 6, 14)
+	fmt.Println()
+	t.Fatal(t.Name())
+	// expected output: 2 5 8 8
+
 }
