c19c1cc1102bb4a6fedc77a78f7a126ba81f4193
SakuraBurst <colapes2@gmail.com>
2021-12-08 17:31:23 +0300

    add node

--- Sprint5/AddNode/addNode.go
+++ Sprint5/AddNode/addNode.go
@@ -1,13 +1,27 @@
 package addNode
 
-import (
-	"io"
-	"bufio"
-	"fmt"
-)
+type Node struct {
+	Value int
+	Left  *Node
+	Right *Node
+}
 
-func AddNode(r io.Reader, w io.Writer){
-	reader := bufio.NewReader(r)
-	//writer := bufio.NewWriter(w)
-	fmt.Println(reader)
+func Insert(root *Node, key int) *Node {
+	// Your code
+	// “ヽ(´▽｀)ノ”
+	if root.Value <= key {
+		if root.Right == nil {
+			root.Right = &Node{Value: key}
+		} else {
+			Insert(root.Right, key)
+		}
+		return root
+	} else {
+		if root.Left == nil {
+			root.Left = &Node{Value: key}
+		} else {
+			Insert(root.Left, key)
+		}
+		return root
+	}
 }
--- Sprint5/AddNode/addNode_test.go
+++ Sprint5/AddNode/addNode_test.go
@@ -1,26 +1,23 @@
 package addNode_test
 
 import (
-	"bytes"
-	"io"
-	"strings"
+	"fmt"
 	"testing"
 
 	addNode "github.com/SakuraBurst/yandex-practicum-go-algorithms/Sprint5/AddNode"
 )
 
-type Test struct {
-	inputData  io.Reader
-	outputData string
-}
-
-func TestAddNode(t *testing.T){
-	tests := []Test{{strings.NewReader(""), ""}}
-	for _, v := range tests {
-		buf := bytes.NewBuffer([]byte{})
-		addNode.AddNode(v.inputData, buf)
-		if buf.String() != v.outputData {
-			t.Errorf("\nexpected:\n%s\ngot:\n%s", v.outputData, buf.String())
-		}
+func TestAddNode(t *testing.T) {
+	node1 := addNode.Node{7, nil, nil}
+	node2 := addNode.Node{8, &node1, nil}
+	node3 := addNode.Node{7, nil, &node2}
+	newHead := addNode.Insert(&node3, 6)
+	fmt.Println(newHead)
+	if newHead != &node3 {
+		t.Errorf("\nHEAD\nexpected:\n%v\ngot:\n%v", &node3, newHead)
 	}
+	if newHead.Left.Value != 6 {
+		t.Errorf("\nValue\nexpected:\n%d\ngot:\n%d", node3.Value, 6)
+	}
+
 }
