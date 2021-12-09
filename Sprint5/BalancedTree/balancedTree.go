a56871739e57fcf46059fc97f6d6da2e698eac74
SakuraBurst <colapes2@gmail.com>
2021-12-09 18:28:15 +0300

    balanced tree

--- Sprint5/BalancedTree/balancedTree.go
+++ Sprint5/BalancedTree/balancedTree.go
@@ -1,13 +1,42 @@
 package balancedTree
 
 import (
-	"io"
-	"bufio"
-	"fmt"
+	"errors"
 )
 
-func BalancedTree(r io.Reader, w io.Writer){
-	reader := bufio.NewReader(r)
-	//writer := bufio.NewWriter(w)
-	fmt.Println(reader)
+type Node struct {
+	Value int
+	Left  *Node
+	Right *Node
+}
+
+func Solution(root *Node) bool {
+	// Your code
+	// “ヽ(´▽｀)ノ”
+	if _, err := counter(root); err != nil {
+		return false
+	}
+	return true
+}
+
+func counter(root *Node) (int, error) {
+	if root == nil {
+		return 0, nil
+	}
+
+	leftTreeLength, leftErr := counter(root.Left)
+	rightTreeLength, rightErr := counter(root.Right)
+	if leftErr != nil || rightErr != nil {
+		return 0, errors.New("tree is not balanced")
+	}
+	res := leftTreeLength - rightTreeLength
+	if res > 1 || res < -1 {
+		return 0, errors.New("tree is not balanced")
+	}
+	if leftTreeLength >= rightTreeLength {
+		return leftTreeLength + 1, nil
+	} else {
+		return rightTreeLength + 1, nil
+	}
+
 }
--- Sprint5/BalancedTree/balancedTree_test.go
+++ Sprint5/BalancedTree/balancedTree_test.go
@@ -1,26 +1,35 @@
 package balancedTree_test
 
 import (
-	"bytes"
-	"io"
-	"strings"
 	"testing"
 
 	balancedTree "github.com/SakuraBurst/yandex-practicum-go-algorithms/Sprint5/BalancedTree"
 )
 
-type Test struct {
-	inputData  io.Reader
-	outputData string
+func TestBalancedTree(t *testing.T) {
+	node1 := balancedTree.Node{1, nil, nil}
+	node2 := balancedTree.Node{-5, nil, nil}
+	node3 := balancedTree.Node{3, &node1, &node2}
+	node4 := balancedTree.Node{10, nil, nil}
+	node5 := balancedTree.Node{2, &node3, &node4}
+	if !balancedTree.Solution(&node5) {
+		t.Errorf("\nexpected:\n%t\ngot:\n%t", true, false)
+	}
+
+	node28 := balancedTree.Node{8, nil, nil}
+	node27 := balancedTree.Node{7, nil, nil}
+	node26 := balancedTree.Node{6, nil, nil}
+	node25 := balancedTree.Node{5, nil, nil}
+	node24 := balancedTree.Node{4, &node27, &node28}
+	node23 := balancedTree.Node{1, &node25, &node26}
+	node22 := balancedTree.Node{1, nil, &node24}
+	node21 := balancedTree.Node{1, &node23, nil}
+	node20 := balancedTree.Node{1, &node21, &node22}
+	if balancedTree.Solution(&node20) {
+		t.Errorf("\nexpected:\n%t\ngot:\n%t", false, true)
+	}
 }
 
-func TestBalancedTree(t *testing.T){
-	tests := []Test{{strings.NewReader(""), ""}}
-	for _, v := range tests {
-		buf := bytes.NewBuffer([]byte{})
-		balancedTree.BalancedTree(v.inputData, buf)
-		if buf.String() != v.outputData {
-			t.Errorf("\nexpected:\n%s\ngot:\n%s", v.outputData, buf.String())
-		}
-	}
+func test() {
+
 }
