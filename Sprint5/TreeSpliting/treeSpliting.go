c4f60f2b4a62e7858ff17259ab854d709b2e219b
SakuraBurst <colapes2@gmail.com>
2021-12-15 02:52:20 +0300

    tree spliting

--- Sprint5/TreeSpliting/treeSpliting.go
+++ Sprint5/TreeSpliting/treeSpliting.go
@@ -1,13 +1,44 @@
 package treeSpliting
 
-import (
-	"io"
-	"bufio"
-	"fmt"
-)
+import "fmt"
 
-func TreeSpliting(r io.Reader, w io.Writer){
-	reader := bufio.NewReader(r)
-	//writer := bufio.NewWriter(w)
-	fmt.Println(reader)
+type Node struct {
+	Value int
+	Left  *Node
+	Right *Node
+	Size  int
+}
+
+func Split(node *Node, k int) (*Node, *Node) {
+	// Your code
+	// “ヽ(´▽｀)ノ”
+	if node == nil {
+		return nil, nil
+	}
+
+	leftSize, rightSize := subTreesLength(node.Left, node.Right)
+	fmt.Println("k = ", k, "current value = ", node.Value, "leftSize = ", leftSize, "right size = ", rightSize)
+	if leftSize+1 > k {
+		ln, rn := Split(node.Left, k)
+		_, rnSize := subTreesLength(ln, rn)
+
+		node.Size = node.Size - leftSize + rnSize
+		node.Left = rn
+		return ln, node
+	}
+	ln, rn := Split(node.Right, k-(leftSize+1))
+	lnSize, _ := subTreesLength(ln, rn)
+	node.Size = node.Size - rightSize + lnSize
+	node.Right = ln
+	return node, rn
+}
+
+func subTreesLength(l, r *Node) (leftSize, rightSize int) {
+	if l != nil {
+		leftSize = l.Size
+	}
+	if r != nil {
+		rightSize = r.Size
+	}
+	return
 }
--- Sprint5/TreeSpliting/treeSpliting_test.go
+++ Sprint5/TreeSpliting/treeSpliting_test.go
@@ -1,26 +1,38 @@
 package treeSpliting_test
 
 import (
-	"bytes"
-	"io"
-	"strings"
+	"fmt"
 	"testing"
 
 	treeSpliting "github.com/SakuraBurst/yandex-practicum-go-algorithms/Sprint5/TreeSpliting"
 )
 
-type Test struct {
-	inputData  io.Reader
-	outputData string
+func TestTreeSpliting(t *testing.T) {
+	node1 := &treeSpliting.Node{3, nil, nil, 1}
+	node2 := &treeSpliting.Node{2, nil, node1, 2}
+	node23 := &treeSpliting.Node{7, nil, nil, 1}
+	node3 := &treeSpliting.Node{8, node23, nil, 2}
+	node4 := &treeSpliting.Node{11, nil, nil, 1}
+	node5 := &treeSpliting.Node{10, node3, node4, 4}
+	node6 := &treeSpliting.Node{5, node2, node5, 7}
+	left, right := treeSpliting.Split(node6, 1)
+	if left.Size != 56 {
+		printTree(left)
+		t.Errorf("\nexpected 4 got %d", left.Size)
+	}
+	fmt.Println()
+	if right.Size != 56 {
+		printTree(right)
+		t.Errorf("\nexpected 2 got %d", right.Size)
+	}
 }
 
-func TestTreeSpliting(t *testing.T){
-	tests := []Test{{strings.NewReader(""), ""}}
-	for _, v := range tests {
-		buf := bytes.NewBuffer([]byte{})
-		treeSpliting.TreeSpliting(v.inputData, buf)
-		if buf.String() != v.outputData {
-			t.Errorf("\nexpected:\n%s\ngot:\n%s", v.outputData, buf.String())
-		}
+func printTree(node *treeSpliting.Node) {
+	if node == nil {
+		return
 	}
+	printTree(node.Left)
+	fmt.Print(node.Value)
+	fmt.Print(" ")
+	printTree(node.Right)
 }
