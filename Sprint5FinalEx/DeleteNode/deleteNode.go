84618a848da8f16807dc4952d5cdf7780dbc342c
SakuraBurst <colapes2@gmail.com>
2021-12-19 00:28:51 +0300

    delete node

--- Sprint5FinalEx/DeleteNode/deleteNode.go
+++ Sprint5FinalEx/DeleteNode/deleteNode.go
@@ -1,13 +1,57 @@
 package deleteNode
 
-import (
-	"io"
-	"bufio"
-	"fmt"
-)
+import "fmt"
 
-func DeleteNode(r io.Reader, w io.Writer){
-	reader := bufio.NewReader(r)
-	//writer := bufio.NewWriter(w)
-	fmt.Println(reader)
+// для тестирования
+type TestNode struct {
+	Value int
+	Left  *TestNode
+	Right *TestNode
+}
+
+// для тестирования
+func Remove(node *TestNode, key int) *TestNode {
+	if node == nil {
+		return node
+	}
+	if node.Value == key {
+		if node.Left == nil && node.Right == nil {
+			return nil
+		}
+
+		if node.Left == nil {
+			return node.Right
+		}
+
+		if node.Right == nil {
+			return node.Left
+		}
+
+		val, replaceNode := getAndDeleteSmallestValInTree(node.Right)
+		fmt.Println(val)
+		node.Right = replaceNode
+		node.Value = val
+		return node
+	}
+	if node.Value > key {
+		newNode := Remove(node.Left, key)
+		node.Left = newNode
+	} else {
+		newNode := Remove(node.Right, key)
+		node.Right = newNode
+	}
+
+	return node
+}
+
+func getAndDeleteSmallestValInTree(n *TestNode) (int, *TestNode) {
+	if n.Left == nil {
+		if n.Right == nil {
+			return n.Value, nil
+		}
+		return n.Value, n.Left
+	}
+	val, replaceNode := getAndDeleteSmallestValInTree(n.Left)
+	n.Left = replaceNode
+	return val, n
 }
--- Sprint5FinalEx/DeleteNode/deleteNode_test.go
+++ Sprint5FinalEx/DeleteNode/deleteNode_test.go
@@ -1,26 +1,46 @@
 package deleteNode_test
 
 import (
-	"bytes"
-	"io"
-	"strings"
 	"testing"
 
 	deleteNode "github.com/SakuraBurst/yandex-practicum-go-algorithms/Sprint5FinalEx/DeleteNode"
 )
 
-type Test struct {
-	inputData  io.Reader
-	outputData string
-}
-
-func TestDeleteNode(t *testing.T){
-	tests := []Test{{strings.NewReader(""), ""}}
-	for _, v := range tests {
-		buf := bytes.NewBuffer([]byte{})
-		deleteNode.DeleteNode(v.inputData, buf)
-		if buf.String() != v.outputData {
-			t.Errorf("\nexpected:\n%s\ngot:\n%s", v.outputData, buf.String())
-		}
+func TestDeleteNode(t *testing.T) {
+	node1 := deleteNode.TestNode{2, nil, nil}
+	node2 := deleteNode.TestNode{3, &node1, nil}
+	node3 := deleteNode.TestNode{1, nil, &node2}
+	node4 := deleteNode.TestNode{6, nil, nil}
+	node5 := deleteNode.TestNode{8, &node4, nil}
+	node6 := deleteNode.TestNode{10, &node5, nil}
+	node7 := deleteNode.TestNode{5, &node3, &node6}
+	newHead := deleteNode.Remove(&node7, 10)
+	if newHead.Value != 5 {
+		t.Errorf("\nnewHead.Value: expect 5 got %d", newHead.Value)
 	}
+	if newHead.Right != &node5 {
+		t.Errorf("\nnewHead.Right: expect %v got %v", &node5, newHead.Right)
+	}
+	if newHead.Right.Value != 8 {
+		t.Errorf("\nnewHead.Right.Value: expect 8 got %d", newHead.Right.Value)
+	}
+
+	node1_1 := deleteNode.TestNode{2, nil, nil}
+	node1_2 := deleteNode.TestNode{3, &node1_1, nil}
+	node1_3 := deleteNode.TestNode{1, nil, &node1_2}
+	node1_4 := deleteNode.TestNode{6, nil, nil}
+	node1_n1 := deleteNode.TestNode{11, nil, nil}
+	node1_n2 := deleteNode.TestNode{16, nil, nil}
+	node1_n3 := deleteNode.TestNode{15, &node1_n1, &node1_n2}
+	node1_5 := deleteNode.TestNode{8, &node1_4, nil}
+	node1_6 := deleteNode.TestNode{10, &node1_5, &node1_n3}
+	node1_7 := deleteNode.TestNode{5, &node1_3, &node1_6}
+	newHead = deleteNode.Remove(&node1_7, 10)
+	if newHead.Value != 5 {
+		t.Errorf("\nnewHead.Value: expect 5 got %d", newHead.Value)
+	}
+	if newHead.Right.Value != 11 {
+		t.Errorf("\nnewHead.Right.Value: expect 11 got %d", newHead.Right.Value)
+	}
+
 }
