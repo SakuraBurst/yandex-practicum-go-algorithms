0ea5918ca01f491ab5869d8cb60682b9d2ecb548
SakuraBurst <colapes2@gmail.com>
2021-12-27 00:19:32 +0300

    time to go out

--- Sprint6/TimeToGetOut/timeToGetOut.go
+++ Sprint6/TimeToGetOut/timeToGetOut.go
@@ -1,13 +1,113 @@
 package timeToGetOut
 
 import (
-	"io"
 	"bufio"
-	"fmt"
+	"errors"
+	"io"
+	"os"
+	"strconv"
+	"strings"
 )
 
-func TimeToGetOut(r io.Reader, w io.Writer){
-	reader := bufio.NewReader(r)
-	//writer := bufio.NewWriter(w)
-	fmt.Println(reader)
+type Node struct {
+	value    int
+	pointsTo AdjacencyList
+}
+
+type AdjacencyList []*Node
+
+func TimeToGetOut(r io.Reader, w io.Writer) {
+	scaner := bufio.NewScanner(r)
+	scaner.Scan()
+	initData := strings.Fields(scaner.Text())
+	n, _ := strconv.Atoi(initData[0])
+	m, _ := strconv.Atoi(initData[1])
+	al := make(AdjacencyList, n+1)
+	for i := 0; i < m; i++ {
+		scaner.Scan()
+		edge := strings.Fields(scaner.Text())
+		vertexAIndex, _ := strconv.Atoi(edge[0])
+		vertexBIndex, _ := strconv.Atoi(edge[1])
+		if al[vertexAIndex] == nil {
+			al[vertexAIndex] = &Node{value: vertexAIndex}
+		}
+		if al[vertexBIndex] == nil {
+			al[vertexBIndex] = &Node{value: vertexBIndex}
+		}
+		al[vertexAIndex].pointsTo = append(al[vertexAIndex].pointsTo, al[vertexBIndex])
+	}
+
+	var dfc func(n *Node) error
+	colors := make([]string, n+1)
+	timer := -1
+	writer := bufio.NewWriter(w)
+	timeIn := make([]string, n+1)
+	timerOut := make([]string, n+1)
+	dfc = func(n *Node) error {
+		if n == nil {
+			return errors.New("node doesen't exist")
+		}
+		timer++
+		colors[n.value] = "black"
+		timeIn[n.value] = strconv.Itoa(timer)
+		n.pointsTo = merge(n.pointsTo)
+		for _, v := range n.pointsTo {
+			if colors[v.value] != "black" {
+				dfc(v)
+			}
+		}
+		timer++
+		timerOut[n.value] = strconv.Itoa(timer)
+		return nil
+
+	}
+	err := dfc(al[1])
+	if err != nil {
+		writer.WriteString("0 1")
+		writer.WriteByte('\n')
+	} else {
+		for i := 1; i < n+1; i++ {
+			writer.WriteString(timeIn[i] + " " + timerOut[i])
+			writer.WriteByte('\n')
+		}
+	}
+
+	writer.Flush()
+
+}
+
+func merge(al AdjacencyList) AdjacencyList {
+	if len(al) <= 1 {
+		return al
+	}
+	mid := len(al) >> 1
+	a := merge(al[:mid])
+	b := merge(al[mid:])
+	aIndex := 0
+	bIndex := 0
+	result := make(AdjacencyList, 0, len(a)+len(b))
+	for aIndex < len(a) || bIndex < len(b) {
+		if aIndex == len(a) {
+			result = append(result, b[bIndex])
+			bIndex++
+			continue
+		}
+		if bIndex == len(b) {
+			result = append(result, a[aIndex])
+			aIndex++
+			continue
+		}
+		if a[aIndex].value <= b[bIndex].value {
+			result = append(result, a[aIndex])
+			aIndex++
+		} else {
+			result = append(result, b[bIndex])
+			bIndex++
+		}
+	}
+	return result
+}
+
+func main() {
+	TimeToGetOut(os.Stdin, os.Stdout)
 }
--- Sprint6/TimeToGetOut/timeToGetOut_test.go
+++ Sprint6/TimeToGetOut/timeToGetOut_test.go
@@ -14,8 +14,31 @@ type Test struct {
 	outputData string
 }
 
-func TestTimeToGetOut(t *testing.T){
-	tests := []Test{{strings.NewReader(""), ""}}
+func TestTimeToGetOut(t *testing.T) {
+	tests := []Test{{strings.NewReader(`6 8
+2 6
+1 6
+3 1
+2 5
+4 3
+3 2
+1 2
+1 4
+`), `0 11
+1 6
+8 9
+7 10
+2 3
+4 5
+`}, {strings.NewReader(`3 2
+1 2
+2 3
+`), `0 5
+1 4
+2 3
+`}, {strings.NewReader(`1 0
+`), `0 1
+`}}
 	for _, v := range tests {
 		buf := bytes.NewBuffer(nil)
 		timeToGetOut.TimeToGetOut(v.inputData, buf)
