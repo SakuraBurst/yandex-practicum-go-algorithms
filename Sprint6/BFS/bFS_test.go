4b35ce4f760652c3b09a2f1c923d4753f3addc67
SakuraBurst <colapes2@gmail.com>
2022-01-05 13:35:11 +0300

    BFS

--- Sprint6/BFS/bFS_test.go
+++ Sprint6/BFS/bFS_test.go
@@ -14,13 +14,38 @@ type Test struct {
 	outputData string
 }
 
-func TestBFS(t *testing.T){
-	tests := []Test{{strings.NewReader(""), ""}}
+func TestBFS(t *testing.T) {
+	tests := []Test{{strings.NewReader(`4 4
+1 2
+2 3
+3 4
+1 4
+3
+`), "3 2 4 1 "}, {strings.NewReader(`2 1
+2 1
+1
+`), "1 2 "}}
 	for _, v := range tests {
 		buf := bytes.NewBuffer(nil)
 		bFS.BFS(v.inputData, buf)
 		if buf.String() != v.outputData {
 			t.Errorf("\nexpected:\n%s\ngot:\n%s", v.outputData, buf.String())
 		}
+	}
+}
+
+func TestQueue_Push(t *testing.T) {
+	testNode := &bFS.Node{
+		Value:    1,
+		PointsTo: nil,
+	}
+	queue := bFS.Queue{
+		CurrentSize:  16,
+		Queue:        make(bFS.AdjacencyList, 16),
+		FirstInQueue: 0,
+		EmptyIndex:   0,
+	}
+	for i := 0; i < 1000; i++ {
+		queue.Push(testNode)
 	}
 }
