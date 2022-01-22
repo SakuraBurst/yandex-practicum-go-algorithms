e48c4ec48732da6cae65c48882bd763f07919ed9
SakuraBurst <colapes2@gmail.com>
2022-01-22 20:52:40 +0300

    railoroads

--- Sprint6FinalEx/Railroads/railroads.go
+++ Sprint6FinalEx/Railroads/railroads.go
@@ -3,106 +3,129 @@ package railroads
 import (
 	"bufio"
 	"io"
+	"os"
 	"strconv"
 	"strings"
 )
 
-type Edge struct {
-	weight int
-	point  *Node
+type WayType uint8
+
+const (
+	UnknownWay WayType = iota
+	RWay
+	BWay
+)
+
+type Matrix [][]WayType
+
+type Roads map[int]WayType
+
+type Edge uint16
+
+type Vertex struct {
+	value    uint16
+	pointsTo []Edge
 }
 
-type PointsTo []Edge
-
-type Node struct {
-	value    int
-	pointsTo PointsTo
-}
-
-type AdjacencyList []*Node
-
-type PriorityQueue []Edge
-
-func (q *PriorityQueue) Push(edge Edge) {
-	*q = append(*q, edge)
-	q.RebalanceTreeUp(q.Len() - 1)
-}
-
-func (q *PriorityQueue) Pop() Edge {
-	// без defer)
-	save := (*q)[1]
-	(*q)[1] = (*q)[q.Len()-1]
-	*q = (*q)[:q.Len()-1]
-	return save
-}
-
-func (q PriorityQueue) RebalanceTreeUp(index int) {
-	if index == 1 {
-		return
-	}
-	parentIndex := index >> 1
-	if q.Less(index, parentIndex) {
-		q.Swap(index, parentIndex)
-		q.RebalanceTreeUp(parentIndex)
-	}
-}
-
-func (q PriorityQueue) RebalanceTreeDown(index int) {
-	firstChild := index * 2
-	secondChild := firstChild + 1
-	if firstChild >= q.Len() {
-		return
-	}
-	best := firstChild
-	if secondChild < q.Len() && q.Less(secondChild, firstChild) {
-		best = secondChild
-	}
-	if q.Less(best, index) {
-		q.Swap(firstChild, index)
-		q.RebalanceTreeDown(firstChild)
-	}
-}
-
-func (q PriorityQueue) Len() int {
-	return len(q)
-}
-
-func (q PriorityQueue) Less(a, b int) bool {
-	return q[a].weight > q[b].weight
-}
-
-func (q PriorityQueue) Swap(a, b int) {
-	q[a], q[b] = q[b], q[a]
-}
+type AdjacencyList []Vertex
 
 func Railroads(r io.Reader, w io.Writer) {
 	scanner := bufio.NewScanner(r)
 	scanner.Scan()
-	initData := strings.Fields(scanner.Text())
-	vertexes, _ := strconv.Atoi(initData[0])
-	edges, _ := strconv.Atoi(initData[1])
-	AL := make(AdjacencyList, vertexes+1)
-	for i := 0; i < vertexes; i++ {
-		alValue := i + 1
-		AL[alValue] = &Node{
-			value:    alValue,
-			pointsTo: nil,
+	N, _ := strconv.Atoi(strings.TrimSpace(scanner.Text()))
+	ALR := make(AdjacencyList, N+1)
+	ALB := make(AdjacencyList, N+1)
+	for i := 1; i < N; i++ {
+		scanner.Scan()
+		railData := scanner.Text()
+		if ALR[i].pointsTo == nil {
+			ALR[i].pointsTo = make([]Edge, 0, N+1-i)
+			ALR[i].value = uint16(i)
+		}
+		if ALB[i].pointsTo == nil {
+			ALB[i].pointsTo = make([]Edge, 0, N+1-i)
+			ALB[i].value = uint16(i)
+		}
+		for g, datum := range railData {
+			if datum == 82 {
+				ALR[i].pointsTo = append(ALR[i].pointsTo, Edge(i+g+1))
+			} else {
+				ALB[i].pointsTo = append(ALB[i].pointsTo, Edge(i+g+1))
+			}
+		}
+
+	}
+	writer := bufio.NewWriter(w)
+	roads := make(Matrix, N+1)
+	for _, vertex := range ALR {
+		if vertex.pointsTo == nil {
+			continue
+		}
+		dfsOnlyCheck(ALR, vertex, nil, roads, RWay)
+	}
+	for _, vertex := range ALB {
+		if vertex.pointsTo == nil {
+			continue
+		}
+		ok := dfs(ALB, vertex, nil, roads, BWay)
+		if !ok {
+			writer.WriteString("NO")
+			writer.Flush()
+			return
 		}
 	}
-	for i := 0; i < edges; i++ {
-		scanner.Scan()
-		edgeData := strings.Fields(scanner.Text())
-		vertexA, _ := strconv.Atoi(edgeData[0])
-		vertexB, _ := strconv.Atoi(edgeData[1])
-		weight, _ := strconv.Atoi(edgeData[2])
-		AL[vertexA].pointsTo = append(AL[vertexA].pointsTo, Edge{
-			weight: weight,
-			point:  AL[vertexB],
-		})
-		AL[vertexB].pointsTo = append(AL[vertexB].pointsTo, Edge{
-			weight: weight,
-			point:  AL[vertexA],
-		})
+
+	writer.WriteString("YES")
+
+	writer.Flush()
+}
+
+func dfsOnlyCheck(AL AdjacencyList, vertex Vertex, citiesWithThatWayType []uint16, roads Matrix, currentWay WayType) {
+	citiesWithThatWayType = append(citiesWithThatWayType, vertex.value)
+Upper:
+	for _, edge := range vertex.pointsTo {
+		for _, i := range citiesWithThatWayType {
+			if roads[edge] == nil {
+				roads[edge] = make([]WayType, cap(roads))
+			}
+			roadValue := roads[edge][i]
+			if roadValue == currentWay {
+				continue Upper
+			}
+
+			roads[edge][i] = currentWay
+		}
+		dfsOnlyCheck(AL, AL[edge], citiesWithThatWayType, roads, currentWay)
 	}
-	//writer := bufio.NewWriter(w)
+}
+
+func dfs(AL AdjacencyList, vertex Vertex, citiesWithThatWayType []uint16, roads Matrix, currentWay WayType) bool {
+	citiesWithThatWayType = append(citiesWithThatWayType, vertex.value)
+Upper:
+	for _, edge := range vertex.pointsTo {
+
+		for _, i := range citiesWithThatWayType {
+			if roads[edge] == nil {
+				roads[edge] = make([]WayType, cap(roads))
+			} else {
+				roadValue := roads[edge][i]
+				if roadValue != UnknownWay && roadValue != currentWay {
+					return false
+				} else if roadValue == currentWay {
+					continue Upper
+				}
+			}
+
+			roads[edge][i] = currentWay
+		}
+
+		ok := dfs(AL, AL[edge], citiesWithThatWayType, roads, currentWay)
+		if !ok {
+			return false
+		}
+	}
+	return true
+}
+func main() {
+	Railroads(os.Stdin, os.Stdout)
 }
--- Sprint6FinalEx/Railroads/railroads_test.go
+++ Sprint6FinalEx/Railroads/railroads_test.go
@@ -3,8 +3,11 @@ package railroads_test
 import (
 	"bytes"
 	"io"
+	"math/rand"
+	"strconv"
 	"strings"
 	"testing"
+	"time"
 
 	railroads "github.com/SakuraBurst/yandex-practicum-go-algorithms/Sprint6FinalEx/Railroads"
 )
@@ -15,12 +18,68 @@ type Test struct {
 }
 
 func TestRailroads(t *testing.T) {
-	tests := []Test{{strings.NewReader(""), ""}}
+	tests := []Test{{strings.NewReader(`3
+RB
+R
+`), "NO"}, {strings.NewReader(`4
+BBB
+RB
+B
+`), "YES"}, {strings.NewReader(`5
+RRRB
+BRR
+BR
+R
+`), "NO"}}
 	for _, v := range tests {
-		buf := bytes.NewBuffer(nil)
-		railroads.Railroads(v.inputData, buf)
-		if buf.String() != v.outputData {
-			t.Errorf("\nexpected:\n%s\ngot:\n%s", v.outputData, buf.String())
-		}
+		t.Run(v.outputData, func(t *testing.T) {
+			buf := bytes.NewBuffer(nil)
+			railroads.Railroads(v.inputData, buf)
+			if buf.String() != v.outputData {
+				t.Errorf("\nexpected:\n%s\ngot:\n%s", v.outputData, buf.String())
+			}
+		})
 	}
+}
+
+func BenchmarkRailroads(b *testing.B) {
+	rand.Seed(time.Now().Unix())
+	str := makeRailData(5000)
+
+	for i := 0; i < b.N; i++ {
+
+		test := Test{
+			inputData:  strings.NewReader(str),
+			outputData: "NO",
+		}
+		buf := bytes.NewBuffer(nil)
+		railroads.Railroads(test.inputData, buf)
+		//log.Print(buf.String())
+	}
+}
+
+func makeRailData(length int) string {
+	var builder strings.Builder
+	builder.WriteString(strconv.Itoa(length))
+	builder.WriteByte('\n')
+	for i := length - 1; i >= 0; i-- {
+		for g := 0; g < i; g++ {
+			if i%2 == 0 {
+				if g%2 == 0 {
+					builder.WriteString("B")
+				} else {
+					builder.WriteString("B")
+				}
+			} else {
+				if g%2 == 0 {
+					builder.WriteString("B")
+				} else {
+					builder.WriteString("B")
+				}
+			}
+
+		}
+		builder.WriteByte('\n')
+	}
+	return builder.String()
 }
