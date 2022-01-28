69631089ad863450340d8e189e2ce20c8873b8f1
SakuraBurst <colapes2@gmail.com>
2022-01-28 17:14:42 +0300

    exhange

--- Sprint7/Exchange/exchange.go
+++ Sprint7/Exchange/exchange.go
@@ -2,12 +2,54 @@ package exchange
 
 import (
 	"bufio"
-	"fmt"
 	"io"
+	"os"
+	"strconv"
+	"strings"
 )
+
+type Price struct {
+	day         int
+	price       int
+	currentPick bool
+}
 
 func Exchange(r io.Reader, w io.Writer) {
 	reader := bufio.NewReader(r)
-	//writer := bufio.NewWriter(w)
-	fmt.Println(reader)
+	nString, _ := reader.ReadString('\n')
+	N, _ := strconv.Atoi(strings.TrimSpace(nString))
+	s, _ := reader.ReadString('\n')
+	initData := strings.Fields(s)
+	priceData := make([]Price, 0, N)
+	for i, datum := range initData {
+		price, _ := strconv.Atoi(datum)
+		priceData = append(priceData, Price{
+			day:   i,
+			price: price,
+		})
+	}
+	var lowPick Price
+	value := 0
+	for i := 0; i < N; i++ {
+		if !lowPick.currentPick {
+			if i+1 < N && priceData[i+1].price < priceData[i].price {
+				continue
+			}
+			lowPick = priceData[i]
+			lowPick.currentPick = true
+			continue
+		}
+		if i+1 < N && priceData[i].price < priceData[i+1].price {
+			continue
+		}
+		value += priceData[i].price - lowPick.price
+		lowPick.currentPick = false
+	}
+	writer := bufio.NewWriter(w)
+	writer.WriteString(strconv.Itoa(value))
+	writer.Flush()
+}
+
+func main() {
+	Exchange(os.Stdin, os.Stdout)
 }
--- Sprint7/Exchange/exchange_test.go
+++ Sprint7/Exchange/exchange_test.go
@@ -15,12 +15,18 @@ type Test struct {
 }
 
 func TestExchange(t *testing.T) {
-	tests := []Test{{strings.NewReader(""), ""}}
+	tests := []Test{{strings.NewReader(`6
+7 1 5 3 6 4`), "7"}, {strings.NewReader(`5
+1 2 3 4 5`), "4"}, {strings.NewReader(`6
+1 12 12 16 1 8`), "22"}}
 	for _, v := range tests {
-		buf := bytes.NewBuffer(nil)
-		exchange.Exchange(v.inputData, buf)
-		if buf.String() != v.outputData {
-			t.Errorf("\nexpected:\n%s\ngot:\n%s", v.outputData, buf.String())
-		}
+		t.Run(v.outputData, func(t *testing.T) {
+			buf := bytes.NewBuffer(nil)
+			exchange.Exchange(v.inputData, buf)
+			if buf.String() != v.outputData {
+				t.Errorf("\nexpected:\n%s\ngot:\n%s", v.outputData, buf.String())
+			}
+		})
+
 	}
 }
