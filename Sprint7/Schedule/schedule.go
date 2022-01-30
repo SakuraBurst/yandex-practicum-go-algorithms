d37dfd219ec789c9f5560bd338ea38faaff5cb1f
SakuraBurst <colapes2@gmail.com>
2022-01-30 15:24:37 +0300

    schedule

--- Sprint7/Schedule/schedule.go
+++ Sprint7/Schedule/schedule.go
@@ -2,12 +2,132 @@ package schedule
 
 import (
 	"bufio"
-	"fmt"
 	"io"
+	"os"
+	"strconv"
+	"strings"
 )
 
+type LessonTime struct {
+	startHour        int
+	startMinute      int
+	endHour          int
+	endMinute        int
+	lessonTimeString string
+}
+
+type AuditorySchedule []LessonTime
+
 func Schedule(r io.Reader, w io.Writer) {
-	reader := bufio.NewReader(r)
-	//writer := bufio.NewWriter(w)
-	fmt.Println(reader)
+	scanner := bufio.NewScanner(r)
+	scanner.Scan()
+	N, _ := strconv.Atoi(scanner.Text())
+	auditorySchedule := make(AuditorySchedule, 0, N)
+	for i := 0; i < N; i++ {
+		scanner.Scan()
+		lessonData := strings.Fields(scanner.Text())
+		startLessonData := strings.Split(lessonData[0], ".")
+		endLessonData := strings.Split(lessonData[1], ".")
+		startHour, _ := strconv.Atoi(startLessonData[0])
+		startMinute := 0
+		if len(startLessonData) > 1 {
+			startMinute, _ = strconv.Atoi(startLessonData[1])
+		}
+		endHour, _ := strconv.Atoi(endLessonData[0])
+		endMinute := 0
+		if len(endLessonData) > 1 {
+			endMinute, _ = strconv.Atoi(endLessonData[1])
+		}
+		auditorySchedule = append(auditorySchedule, LessonTime{
+			startHour:        startHour,
+			startMinute:      startMinute,
+			endHour:          endHour,
+			endMinute:        endMinute,
+			lessonTimeString: scanner.Text(),
+		})
+	}
+	auditorySchedule = merge(auditorySchedule)
+	var lastLessonTime *LessonTime
+	writer := bufio.NewWriter(w)
+	counter := 0
+	stringBuilder := strings.Builder{}
+	for i, time := range auditorySchedule {
+		if lastLessonTime != nil {
+			if time.startHour < lastLessonTime.endHour {
+				continue
+			}
+			if time.startHour == lastLessonTime.endHour {
+				if time.startMinute < lastLessonTime.endMinute {
+					continue
+				}
+			}
+		}
+		counter++
+		stringBuilder.WriteString(time.lessonTimeString)
+		stringBuilder.WriteByte('\n')
+		lastLessonTime = &auditorySchedule[i]
+	}
+	writer.WriteString(strconv.Itoa(counter))
+	writer.WriteByte('\n')
+	writer.WriteString(stringBuilder.String())
+	writer.Flush()
+}
+
+func merge(schedule AuditorySchedule) AuditorySchedule {
+	scheduleLength := len(schedule)
+	if scheduleLength < 2 {
+		return schedule
+	}
+	mid := scheduleLength >> 1
+	a := merge(schedule[:mid])
+	aLength := len(a)
+	b := merge(schedule[mid:])
+	bLength := len(b)
+	aIndex := 0
+	bIndex := 0
+	result := make(AuditorySchedule, 0, scheduleLength)
+	for aIndex < aLength || bIndex < bLength {
+		if aIndex == aLength {
+			result = append(result, b[bIndex])
+			bIndex++
+			continue
+		}
+		if bIndex == bLength {
+			result = append(result, a[aIndex])
+			aIndex++
+			continue
+		}
+		if LessonTimeLess(a[aIndex], b[bIndex]) {
+			result = append(result, a[aIndex])
+			aIndex++
+		} else {
+			result = append(result, b[bIndex])
+			bIndex++
+		}
+	}
+	return result
+}
+
+func LessonTimeLess(a, b LessonTime) bool {
+	if a.endHour == b.endHour {
+		if a.endMinute == b.endMinute {
+			return StartLess(a, b)
+		}
+		return a.endMinute < b.endMinute
+	}
+	return a.endHour < b.endHour
+
+}
+
+func StartLess(a, b LessonTime) bool {
+	if a.startHour == b.startHour {
+		return a.startMinute < b.startMinute
+	}
+
+	return a.startHour < b.startHour
+
+}
+
+func main() {
+	Schedule(os.Stdin, os.Stdout)
 }
--- Sprint7/Schedule/schedule_test.go
+++ Sprint7/Schedule/schedule_test.go
@@ -15,12 +15,44 @@ type Test struct {
 }
 
 func TestSchedule(t *testing.T) {
-	tests := []Test{{strings.NewReader(""), ""}}
+	tests := []Test{{strings.NewReader(`5
+9 10
+9.3 10.3
+10 11
+10.3 11.3
+11 12
+`), `3
+9 10
+10 11
+11 12
+`}, {strings.NewReader(`3
+9 10
+11 12.25
+12.15 13.3
+`), `2
+9 10
+11 12.25
+`}, {strings.NewReader(`7
+19 19
+7 14
+12 14
+8 22
+22 23
+5 21
+9 23
+`), `3
+7 14
+19 19
+22 23
+`}}
 	for _, v := range tests {
-		buf := bytes.NewBuffer(nil)
-		schedule.Schedule(v.inputData, buf)
-		if buf.String() != v.outputData {
-			t.Errorf("\nexpected:\n%s\ngot:\n%s", v.outputData, buf.String())
-		}
+		t.Run(v.outputData, func(t *testing.T) {
+			buf := bytes.NewBuffer(nil)
+			schedule.Schedule(v.inputData, buf)
+			if buf.String() != v.outputData {
+				t.Errorf("\nexpected:\n%s\ngot:\n%s", v.outputData, buf.String())
+			}
+		})
+
 	}
 }
