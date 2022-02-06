e335f7bc19981201d95b3309299e889feb58a02f
SakuraBurst <colapes2@gmail.com>
2022-02-06 17:40:46 +0300

    leprechaunt gold

--- Sprint7/LeprechaunGold/leprechaunGold.go
+++ Sprint7/LeprechaunGold/leprechaunGold.go
@@ -2,12 +2,58 @@ package leprechaunGold
 
 import (
 	"bufio"
-	"fmt"
 	"io"
+	"os"
+	"strconv"
+	"strings"
 )
+
+type GoldHeap struct {
+	weight  int
+	canTake []int
+}
 
 func LeprechaunGold(r io.Reader, w io.Writer) {
 	reader := bufio.NewReader(r)
-	//writer := bufio.NewWriter(w)
-	fmt.Println(reader)
+	initDataString, _ := reader.ReadString('\n')
+	initData := strings.Fields(initDataString)
+	n, _ := strconv.Atoi(initData[0])
+	M, _ := strconv.Atoi(initData[1])
+	mainDataString, _ := reader.ReadString('\n')
+	mainData := strings.Fields(mainDataString)
+	previousGoldHeap := GoldHeap{
+		weight:  0,
+		canTake: make([]int, M+1),
+	}
+	var goldHeap GoldHeap
+	for i := 0; i < n; i++ {
+		weight, _ := strconv.Atoi(mainData[i])
+		goldHeap = GoldHeap{
+			weight:  weight,
+			canTake: make([]int, M+1),
+		}
+		for g := 0; g < M+1; g++ {
+			if g-weight >= 0 {
+				goldHeap.canTake[g] = max(previousGoldHeap.canTake[g], weight+previousGoldHeap.canTake[g-weight])
+			} else {
+				goldHeap.canTake[g] = previousGoldHeap.canTake[g]
+			}
+
+		}
+		previousGoldHeap = goldHeap
+
+	}
+	writer := bufio.NewWriter(w)
+	writer.WriteString(strconv.Itoa(goldHeap.canTake[M]))
+	writer.Flush()
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+func main() {
+	LeprechaunGold(os.Stdin, os.Stdout)
 }
--- Sprint7/LeprechaunGold/leprechaunGold_test.go
+++ Sprint7/LeprechaunGold/leprechaunGold_test.go
@@ -15,12 +15,19 @@ type Test struct {
 }
 
 func TestLeprechaunGold(t *testing.T) {
-	tests := []Test{{strings.NewReader(""), ""}}
+	tests := []Test{{strings.NewReader(`5 15
+3 8 1 2 5
+`), "15"}, {strings.NewReader(`5 19
+10 10 7 7 4
+`), "18"}}
 	for _, v := range tests {
-		buf := bytes.NewBuffer(nil)
-		leprechaunGold.LeprechaunGold(v.inputData, buf)
-		if buf.String() != v.outputData {
-			t.Errorf("\nexpected:\n%s\ngot:\n%s", v.outputData, buf.String())
-		}
+		t.Run(v.outputData, func(t *testing.T) {
+			buf := bytes.NewBuffer(nil)
+			leprechaunGold.LeprechaunGold(v.inputData, buf)
+			if buf.String() != v.outputData {
+				t.Errorf("\nexpected:\n%s\ngot:\n%s", v.outputData, buf.String())
+			}
+		})
+
 	}
 }
