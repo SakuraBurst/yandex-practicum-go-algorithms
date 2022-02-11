10e151b56d172c60ea31b5ac72e9fb7e66e489d2
SakuraBurst <colapes2@gmail.com>
2022-02-12 02:59:03 +0300

    atm

--- Sprint7/Atm/atm.go
+++ Sprint7/Atm/atm.go
@@ -4,10 +4,58 @@ import (
 	"bufio"
 	"fmt"
 	"io"
+	"os"
+	"strconv"
+	"strings"
 )
 
+type Matrix [][]int
+
+func (m Matrix) String() string {
+	s := ""
+	for _, ints := range m {
+		s += fmt.Sprint(ints)
+		s += string('\n')
+	}
+	return s
+}
+
 func Atm(r io.Reader, w io.Writer) {
-	reader := bufio.NewReader(r)
-	//writer := bufio.NewWriter(w)
-	fmt.Println(reader)
+	scanner := bufio.NewScanner(r)
+	scanner.Scan()
+	m, _ := strconv.Atoi(scanner.Text())
+	scanner.Scan()
+	n, _ := strconv.Atoi(scanner.Text())
+	scanner.Scan()
+	atmData := strings.Fields(scanner.Text())
+	atm := make([]int, 0, n)
+	for _, bankNoteString := range atmData {
+		bankNote, _ := strconv.Atoi(bankNoteString)
+		atm = append(atm, bankNote)
+	}
+	matrix := make(Matrix, n+1)
+	for i := 0; i < n+1; i++ {
+		matrix[i] = make([]int, m+1)
+		if i == 0 {
+			continue
+		}
+		if atm[i-1] < m+1 {
+			matrix[i][atm[i-1]] = 1
+		}
+		for g := 0; g < m+1; g++ {
+
+			matrix[i][g] += matrix[i-1][g]
+
+			if g-atm[i-1] >= 0 && matrix[i][g-atm[i-1]] > 0 {
+				matrix[i][g] += matrix[i][g-atm[i-1]]
+			}
+		}
+	}
+	writer := bufio.NewWriter(w)
+	writer.WriteString(strconv.Itoa(matrix[n][m]))
+	writer.Flush()
+}
+
+func main() {
+	Atm(os.Stdin, os.Stdout)
 }
--- Sprint7/Atm/atm_test.go
+++ Sprint7/Atm/atm_test.go
@@ -15,12 +15,22 @@ type Test struct {
 }
 
 func TestAtm(t *testing.T) {
-	tests := []Test{{strings.NewReader(""), ""}}
+	tests := []Test{{strings.NewReader(`5
+3
+3 2 1`), "5"}, {strings.NewReader(`3
+2
+2 1`), "2"}, {strings.NewReader(`8
+1
+5`), "0"}, {strings.NewReader(`9
+7
+10 2 3 7 6 5 9`), "6"}}
 	for _, v := range tests {
-		buf := bytes.NewBuffer(nil)
-		atm.Atm(v.inputData, buf)
-		if buf.String() != v.outputData {
-			t.Errorf("\nexpected:\n%s\ngot:\n%s", v.outputData, buf.String())
-		}
+		t.Run(v.outputData, func(t *testing.T) {
+			buf := bytes.NewBuffer(nil)
+			atm.Atm(v.inputData, buf)
+			if buf.String() != v.outputData {
+				t.Errorf("\nexpected:\n%s\ngot:\n%s", v.outputData, buf.String())
+			}
+		})
 	}
 }
