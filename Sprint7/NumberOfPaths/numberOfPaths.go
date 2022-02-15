6744928e977331d75a04252b249fb6ea82d29e76
SakuraBurst <colapes2@gmail.com>
2022-02-15 16:41:48 +0300

    Number of paths

--- Sprint7/NumberOfPaths/numberOfPaths.go
+++ Sprint7/NumberOfPaths/numberOfPaths.go
@@ -2,12 +2,61 @@ package numberOfPaths
 
 import (
 	"bufio"
-	"fmt"
 	"io"
+	"os"
+	"strconv"
+	"strings"
 )
 
+const Mod = 1000000007
+
+type Vertex struct {
+	value    int
+	pointsTo []*Vertex
+}
+
+type AdjacencyList []Vertex
+
 func NumberOfPaths(r io.Reader, w io.Writer) {
-	reader := bufio.NewReader(r)
-	//writer := bufio.NewWriter(w)
-	fmt.Println(reader)
+	scanner := bufio.NewScanner(r)
+	scanner.Scan()
+	initData := strings.Fields(scanner.Text())
+	n, _ := strconv.Atoi(initData[0])
+	m, _ := strconv.Atoi(initData[1])
+	AL := make(AdjacencyList, n+1)
+	for i := 1; i < n+1; i++ {
+		AL[i].value = i
+	}
+	for i := 0; i < m; i++ {
+		scanner.Scan()
+		edgeData := strings.Fields(scanner.Text())
+		edgeA, _ := strconv.Atoi(edgeData[0])
+		edgeB, _ := strconv.Atoi(edgeData[1])
+		AL[edgeA].pointsTo = append(AL[edgeA].pointsTo, &AL[edgeB])
+	}
+	scanner.Scan()
+	searchData := strings.Fields(scanner.Text())
+	searchFrom, _ := strconv.Atoi(searchData[0])
+	searchTo, _ := strconv.Atoi(searchData[1])
+	paths := make([]int, n+1)
+	visited := make([]bool, n+1)
+	paths[searchTo] = 1
+	dfs(&AL[searchFrom], visited, paths)
+	writer := bufio.NewWriter(w)
+	writer.WriteString(strconv.Itoa(paths[searchFrom]))
+	writer.Flush()
+}
+
+func dfs(vertex *Vertex, visited []bool, paths []int) {
+	visited[vertex.value] = true
+	for _, v := range vertex.pointsTo {
+		if !visited[v.value] {
+			dfs(v, visited, paths)
+		}
+		paths[vertex.value] = (paths[vertex.value] + paths[v.value]) % Mod
+	}
+}
+
+func main() {
+	NumberOfPaths(os.Stdin, os.Stdout)
 }
--- Sprint7/NumberOfPaths/numberOfPaths_test.go
+++ Sprint7/NumberOfPaths/numberOfPaths_test.go
@@ -15,12 +15,51 @@ type Test struct {
 }
 
 func TestNumberOfPaths(t *testing.T) {
-	tests := []Test{{strings.NewReader(""), ""}}
+	tests := []Test{{strings.NewReader(`3 3
+1 2
+1 2
+2 3
+1 3
+`), "2"}, {strings.NewReader(`5 3
+1 2
+3 4
+4 5
+1 5
+`), "0"}, {strings.NewReader(`3 3
+1 2
+2 3
+1 3
+1 1
+`), "1"}, {strings.NewReader(`10 20
+5 4
+2 10
+2 10
+2 3
+4 1
+5 1
+10 6
+4 1
+1 6
+2 10
+4 6
+9 6
+9 10
+2 7
+4 1
+9 1
+7 3
+2 9
+10 4
+2 3
+9 1`), "4"}}
 	for _, v := range tests {
-		buf := bytes.NewBuffer(nil)
-		numberOfPaths.NumberOfPaths(v.inputData, buf)
-		if buf.String() != v.outputData {
-			t.Errorf("\nexpected:\n%s\ngot:\n%s", v.outputData, buf.String())
-		}
+		t.Run(v.outputData, func(t *testing.T) {
+			buf := bytes.NewBuffer(nil)
+			numberOfPaths.NumberOfPaths(v.inputData, buf)
+			if buf.String() != v.outputData {
+				t.Errorf("\nexpected:\n%s\ngot:\n%s", v.outputData, buf.String())
+			}
+		})
+
 	}
 }
