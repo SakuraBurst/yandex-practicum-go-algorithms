9b1cb698f6fbf970e5b277284ef431e014c5b2d2
SakuraBurst <colapes2@gmail.com>
2022-03-08 20:48:49 +0300

    border control

--- Sprint8/BorderControl/borderControl.go
+++ Sprint8/BorderControl/borderControl.go
@@ -2,12 +2,60 @@ package borderControl
 
 import (
 	"bufio"
-	"fmt"
 	"io"
+	"os"
+	"strings"
 )
 
 func BorderControl(r io.Reader, w io.Writer) {
 	reader := bufio.NewReader(r)
-	//writer := bufio.NewWriter(w)
-	fmt.Println(reader)
+	max, _ := reader.ReadString('\n')
+	max = strings.TrimSpace(max)
+	min, _ := reader.ReadString('\n')
+	min = strings.TrimSpace(min)
+	if len(max) < len(min) {
+		max, min = min, max
+	}
+	if len(max)-len(min) > 2 {
+		io.WriteString(w, "FAIL")
+		return
+	}
+	i := 0
+	g := 0
+	diff := 0
+	for i < len(max) && g < len(min) {
+		if g == len(min) {
+			diff++
+			i++
+			if diff > 1 {
+				io.WriteString(w, "FAIL")
+				return
+			}
+			continue
+		}
+		if max[i] != min[g] {
+			diff++
+			if diff > 1 {
+				io.WriteString(w, "FAIL")
+				return
+			}
+			if max[i+1] == min[g] {
+				i++
+			} else if max[i+1] == min[g+1] {
+				i++
+				g++
+			} else {
+				io.WriteString(w, "FAIL")
+				return
+			}
+			continue
+		}
+		i++
+		g++
+	}
+	io.WriteString(w, "OK")
+}
+
+func main() {
+	BorderControl(os.Stdin, os.Stdout)
 }
--- Sprint8/BorderControl/borderControl_test.go
+++ Sprint8/BorderControl/borderControl_test.go
@@ -15,12 +15,23 @@ type Test struct {
 }
 
 func TestBorderControl(t *testing.T) {
-	tests := []Test{{strings.NewReader(""), ""}}
+	tests := []Test{{strings.NewReader(`abcdefg
+abdefg
+`), "OK"}, {strings.NewReader(`helo
+hello
+`), "OK"}, {strings.NewReader(`dog
+fog
+`), "OK"}, {strings.NewReader(`mama
+papa
+`), "FAIL"}}
 	for _, v := range tests {
-		buf := bytes.NewBuffer(nil)
-		borderControl.BorderControl(v.inputData, buf)
-		if buf.String() != v.outputData {
-			t.Errorf("\nexpected:\n%s\ngot:\n%s", v.outputData, buf.String())
-		}
+		t.Run(v.outputData, func(t *testing.T) {
+			buf := bytes.NewBuffer(nil)
+			borderControl.BorderControl(v.inputData, buf)
+			if buf.String() != v.outputData {
+				t.Errorf("\nexpected:\n%s\ngot:\n%s", v.outputData, buf.String())
+			}
+		})
+
 	}
 }
