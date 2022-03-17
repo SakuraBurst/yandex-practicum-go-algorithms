1ce2206bb57336d6b1dc87c127eeef3a7536e72c
SakuraBurst <colapes2@gmail.com>
2022-03-17 18:59:33 +0300

    perfix function (сложна)

--- Sprint8/CountingThePrefixFunction/countingThePrefixFunction.go
+++ Sprint8/CountingThePrefixFunction/countingThePrefixFunction.go
@@ -2,12 +2,34 @@ package countingThePrefixFunction
 
 import (
 	"bufio"
-	"fmt"
 	"io"
+	"os"
+	"strconv"
 )
 
 func CountingThePrefixFunction(r io.Reader, w io.Writer) {
 	reader := bufio.NewReader(r)
-	//writer := bufio.NewWriter(w)
-	fmt.Println(reader)
+	s, _ := reader.ReadString('\n')
+	s = s[:len(s)-1]
+	dp := make([]int, len(s))
+	writer := bufio.NewWriter(w)
+	writer.WriteString("0 ")
+	for i := 1; i < len(s); i++ {
+		k := dp[i-1]
+		for k > 0 && s[i] != s[k] {
+			k = dp[k-1]
+		}
+		if s[i] == s[k] {
+			dp[i] = k + 1
+			writer.WriteString(strconv.Itoa(dp[i]) + " ")
+		} else {
+			dp[i] = 0
+			writer.WriteString("0 ")
+		}
+	}
+	writer.Flush()
+}
+
+func main() {
+	CountingThePrefixFunction(os.Stdin, os.Stdout)
 }
