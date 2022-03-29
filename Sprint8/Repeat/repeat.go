e8103d9ca698e2af85615ee2af5e020920ba59ea
SakuraBurst <colapes2@gmail.com>
2022-03-29 19:27:16 +0300

    repeat

--- Sprint8/Repeat/repeat.go
+++ Sprint8/Repeat/repeat.go
@@ -2,12 +2,36 @@ package repeat
 
 import (
 	"bufio"
-	"fmt"
 	"io"
+	"os"
+	"strconv"
 )
 
 func Repeat(r io.Reader, w io.Writer) {
 	reader := bufio.NewReader(r)
-	//writer := bufio.NewWriter(w)
-	fmt.Println(reader)
+	s, _ := reader.ReadString('\n')
+	s = s[:len(s)-1]
+	prefix := string(s[0])
+	prefixLength := len(prefix)
+	i := 1
+
+	for i+prefixLength <= len(s) {
+		if prefix == s[i:i+prefixLength] {
+			i += prefixLength
+		} else {
+			i++
+			prefix = s[:i]
+			prefixLength = len(prefix)
+		}
+	}
+	if len(prefix) > len(s)/2 {
+		prefix = s
+		prefixLength = len(prefix)
+	}
+	io.WriteString(w, strconv.Itoa(len(s)/prefixLength))
+
+}
+
+func main() {
+	Repeat(os.Stdin, os.Stdout)
 }
--- Sprint8/Repeat/repeat_test.go
+++ Sprint8/Repeat/repeat_test.go
@@ -15,12 +15,20 @@ type Test struct {
 }
 
 func TestRepeat(t *testing.T) {
-	tests := []Test{{strings.NewReader(""), ""}}
+	tests := []Test{
+		{strings.NewReader("zzzzzz\n"), "6"},
+		{strings.NewReader("abacaba\n"), "1"},
+		{strings.NewReader("abababab\n"), "4"},
+		{strings.NewReader("tuhhbntuhhbntuhhbn\n"), "3"},
+	}
 	for _, v := range tests {
-		buf := bytes.NewBuffer(nil)
-		repeat.Repeat(v.inputData, buf)
-		if buf.String() != v.outputData {
-			t.Errorf("\nexpected:\n%s\ngot:\n%s", v.outputData, buf.String())
-		}
+		t.Run(v.outputData, func(t *testing.T) {
+			buf := bytes.NewBuffer(nil)
+			repeat.Repeat(v.inputData, buf)
+			if buf.String() != v.outputData {
+				t.Errorf("\nexpected:\n%s\ngot:\n%s", v.outputData, buf.String())
+			}
+		})
+
 	}
 }
